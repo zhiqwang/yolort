033257d1382499a08652431459246fb0a4184640
zhiqwang <zhiqwang@outlook.com>
2020-11-02 07:33:55 -0500

    Add Image preprocess and postprocess Class to Model

--- hubconf.py
+++ hubconf.py
@@ -1,4 +1,5 @@
 import torch
+from torchvision.models.detection.transform import GeneralizedRCNNTransform
 
 from models.yolo import Model
 from models.box_head import YoloHead, PostProcess
@@ -6,6 +7,8 @@ from models.yolo_wrapped import YoloBody, YOLO
 
 
 def yolov5(cfg_path='./models/yolov5s.yaml', checkpoint_path=None):
+    min_size, max_size, image_mean, image_std = 352, 416, [0, 0, 0], [1, 1, 1]
+    transform = GeneralizedRCNNTransform(min_size, max_size, image_mean, image_std)
     backbone = Model(cfg=cfg_path)
     layer_body = YoloBody(yolo_body=backbone, return_layers={'17': '0', '20': '1', '23': '2'})
 
@@ -18,7 +21,7 @@ def yolov5(cfg_path='./models/yolov5s.yaml', checkpoint_path=None):
     layer_box_head = YoloHead(*args_detect)
     post_process = PostProcess(conf_thres=0.4, iou_thres=0.5)
 
-    model = YOLO(layer_body, layer_box_head, post_process, [8., 16., 32.])
+    model = YOLO(layer_body, layer_box_head, post_process, transform, [8., 16., 32.])
 
     if checkpoint_path is not None:
         checkpoint = torch.load(checkpoint_path, map_location="cpu")
--- models/yolo_wrapped.py
+++ models/yolo_wrapped.py
@@ -6,7 +6,7 @@ from typing import Union
 import torch
 from torch import nn, Tensor
 
-from torch.jit.annotations import List, Dict
+from torch.jit.annotations import Tuple, List, Dict, Optional
 
 from utils.general import check_anchor_order
 
@@ -17,9 +17,11 @@ class YOLO(nn.Module):
         body: nn.Module,
         box_head: nn.Module,
         post_process: nn.Module,
+        transform: nn.Module,
         stride: List[float] = [8., 16., 32.],
     ):
         super().__init__()
+        self.transform = transform
         self.body = body
         box_head.stride = torch.tensor(stride)
         box_head.anchors /= box_head.stride.view(-1, 1, 1)
@@ -40,10 +42,35 @@ class YOLO(nn.Module):
 
         return detections
 
-    def forward(self, samples: Tensor):
-        features = self.body(samples)
+    def forward(
+        self,
+        images: List[Tensor],
+        targets: Optional[List[Dict[str, Tensor]]] = None,
+    ) -> Tuple[List[Tensor], List[Dict[str, Tensor]]]:
+        """
+        Arguments:
+            images (list[Tensor]): images to be processed
+            targets (list[Dict[Tensor]]): ground-truth boxes present in the image (optional)
+
+        Returns:
+            result (list[BoxList] or dict[Tensor]): the output from the model.
+                During Training, it returns a dict[Tensor] which contains the losses
+                TODO, currently this repo doesn't support training.
+                During Testing, it returns list[BoxList] contains additional fields
+                like `scores` and `labels`.
+        """
+        original_image_sizes = torch.jit.annotate(List[Tuple[int, int]], [])
+        for img in images:
+            val = img.shape[-2:]
+            assert len(val) == 2
+            original_image_sizes.append((val[0], val[1]))
+
+        images, targets = self.transform(images, targets)
+
+        features = self.body(images.tensors)
         predictions = self.box_head(features)
         detections = self.post_process(predictions)
+        detections = self.transform.postprocess(detections, images.image_sizes, original_image_sizes)
 
         if torch.jit.is_scripting():
             if not self._has_warned:
