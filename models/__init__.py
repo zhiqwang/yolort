1ea1a5430dc39777b5c0ca3e52119424e302c6c2
zhiqwang <zhiqwang@outlook.com>
2020-11-04 04:10:31 -0500

    Refactoring torch hubconf

---
+++ models/__init__.py
@@ -0,0 +1,30 @@
+import torch
+from torchvision.models.detection.transform import GeneralizedRCNNTransform
+
+from .yolo import Model
+from .box_head import YoloHead, PostProcess
+from .yolo_wrapped import YoloBody, YOLO
+
+
+def yolov5(cfg_path='./models/yolov5s.yaml', checkpoint_path=None):
+    min_size, max_size, image_mean, image_std = 320, 416, [0, 0, 0], [1, 1, 1]
+    transform = GeneralizedRCNNTransform(min_size, max_size, image_mean, image_std)
+    backbone = Model(cfg=cfg_path)
+    layer_body = YoloBody(yolo_body=backbone, return_layers={'17': '0', '20': '1', '23': '2'})
+
+    args_detect = [
+        80,
+        [8., 16., 32.],
+        [[10, 13, 16, 30, 33, 23], [30, 61, 62, 45, 59, 119], [116, 90, 156, 198, 373, 326]],
+        [128, 256, 512],
+    ]
+    layer_box_head = YoloHead(*args_detect)
+    post_process = PostProcess(conf_thres=0.4, iou_thres=0.5)
+
+    model = YOLO(layer_body, layer_box_head, post_process, transform, [8., 16., 32.])
+
+    if checkpoint_path is not None:
+        checkpoint = torch.load(checkpoint_path, map_location="cpu")
+        model.load_state_dict(checkpoint)
+
+    return model
