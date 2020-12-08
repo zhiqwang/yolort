1e77dda3fd58560b29072622f084cf80773267e8
zhiqwang <zhiqwang@outlook.com>
2020-12-08 12:10:35 -0500

    Add loss computation tools

--- models/box_head.py
+++ models/box_head.py
@@ -6,6 +6,7 @@ from torch.jit.annotations import Tuple, List, Dict, Optional
 from torchvision.ops import batched_nms, box_iou
 
 from . import _utils as det_utils
+from utils.box_ops import bbox_iou, FocalLoss
 
 
 def _sum(x: List[Tensor]) -> Tensor:
@@ -59,6 +60,11 @@ class YoloHead(nn.Module):
         return torch.cat(all_pred_logits, dim=1)
 
 
+def smooth_BCE(eps=0.1):  # https://github.com/ultralytics/yolov3/issues/238#issuecomment-598028441
+    # return positive, negative label smoothing BCE targets
+    return 1.0 - 0.5 * eps, 0.5 * eps
+
+
 class SetCriterion(nn.Module):
     """This class computes the loss for YOLOv5.
     Arguments:
@@ -74,6 +80,13 @@ class SetCriterion(nn.Module):
         weights: Tuple[float, float, float, float] = (1.0, 1.0, 1.0, 1.0),
         fg_iou_thresh: float = 0.5,
         bg_iou_thresh: float = 0.4,
+        box: float = 0.05,  # box loss gain
+        cls: float = 0.5,  # cls loss gain
+        cls_pw: float = 1.0,  # cls BCELoss positive_weight
+        obj: float = 1.0,  # obj loss gain (scale with pixels)
+        obj_pw: float = 1.0,  # obj BCELoss positive_weight
+        anchor_t: float = 1.0,
+        fl_gamma: float = 0.0,  # focal loss gamma
         allow_low_quality_matches: bool = True,
     ) -> None:
         """
@@ -131,17 +144,17 @@ class SetCriterion(nn.Module):
         """
         device = anchors.device
         lcls, lbox, lobj = torch.zeros(1, device=device), torch.zeros(1, device=device), torch.zeros(1, device=device)
-        h = model.hyp  # hyperparameters
+        tcls, tbox, indices, anchors = self.build_targets(bbox_regression, targets)  # targets
 
         # Define criteria
-        BCEcls = nn.BCEWithLogitsLoss(pos_weight=torch.Tensor([h['cls_pw']])).to(device)
-        BCEobj = nn.BCEWithLogitsLoss(pos_weight=torch.Tensor([h['obj_pw']])).to(device)
+        BCEcls = nn.BCEWithLogitsLoss(pos_weight=torch.Tensor([self.cls_pw])).to(device)
+        BCEobj = nn.BCEWithLogitsLoss(pos_weight=torch.Tensor([self.obj_pw])).to(device)
 
         # Class label smoothing https://arxiv.org/pdf/1902.04103.pdf eqn 3
         cp, cn = smooth_BCE(eps=0.0)
 
         # Focal loss
-        g = h['fl_gamma']  # focal loss gamma
+        g = self.fl_gamma  # focal loss gamma
         if g > 0:
             BCEcls, BCEobj = FocalLoss(BCEcls, g), FocalLoss(BCEobj, g)
 
@@ -181,13 +194,68 @@ class SetCriterion(nn.Module):
             lobj += BCEobj(pi[..., 4], tobj) * balance[i]  # obj loss
 
         s = 3 / no  # output count scaling
-        lbox *= h['box'] * s
-        lobj *= h['obj'] * s * (1.4 if no == 4 else 1.)
-        lcls *= h['cls'] * s
+        lbox *= self.box * s
+        lobj *= self.obj * s * (1.4 if no == 4 else 1.)
+        lcls *= self.cls * s
         bs = tobj.shape[0]  # batch size
 
         loss = lbox + lobj + lcls
         return loss * bs, torch.cat((lbox, lobj, lcls, loss)).detach()
+
+    def build_targets(self, predictions, targets):
+        # Build targets for compute_loss(), input targets(image,class,x,y,w,h)
+        na, nt = det.na, targets.shape[0]  # number of anchors, targets
+        tcls, tbox, indices, anch = [], [], [], []
+        gain = torch.ones(7, device=targets.device)  # normalized to gridspace gain
+        ai = torch.arange(na, device=targets.device).float().view(na, 1).repeat(1, nt)  # same as .repeat_interleave(nt)
+        targets = torch.cat((targets.repeat(na, 1, 1), ai[:, :, None]), 2)  # append anchor indices
+
+        g = 0.5  # bias
+        off = torch.tensor([[0, 0],
+                            [1, 0], [0, 1], [-1, 0], [0, -1],  # j,k,l,m
+                            # [1, 1], [1, -1], [-1, 1], [-1, -1],  # jk,jm,lk,lm
+                            ], device=targets.device).float() * g  # offsets
+
+        for i in range(det.nl):
+            anchors = det.anchors[i]
+            gain[2:6] = torch.tensor(predictions[i].shape)[[3, 2, 3, 2]]  # xyxy gain
+
+            # Match targets to anchors
+            t = targets * gain
+            if nt:
+                # Matches
+                r = t[:, :, 4:6] / anchors[:, None]  # wh ratio
+                j = torch.max(r, 1. / r).max(2)[0] < self.anchor_t  # compare
+                # j = wh_iou(anchors, t[:, 4:6]) > model.hyp['iou_t']  # iou(3,n)=wh_iou(anchors(3,2), gwh(n,2))
+                t = t[j]  # filter
+
+                # Offsets
+                gxy = t[:, 2:4]  # grid xy
+                gxi = gain[[2, 3]] - gxy  # inverse
+                j, k = ((gxy % 1. < g) & (gxy > 1.)).T
+                l, m = ((gxi % 1. < g) & (gxi > 1.)).T
+                j = torch.stack((torch.ones_like(j), j, k, l, m))
+                t = t.repeat((5, 1, 1))[j]
+                offsets = (torch.zeros_like(gxy)[None] + off[:, None])[j]
+            else:
+                t = targets[0]
+                offsets = 0
+
+            # Define
+            b, c = t[:, :2].long().T  # image, class
+            gxy = t[:, 2:4]  # grid xy
+            gwh = t[:, 4:6]  # grid wh
+            gij = (gxy - offsets).long()
+            gi, gj = gij.T  # grid xy indices
+
+            # Append
+            a = t[:, 6].long()  # anchor indices
+            indices.append((b, a, gj.clamp_(0, gain[3] - 1), gi.clamp_(0, gain[2] - 1)))  # image, anchor, grid indices
+            tbox.append(torch.cat((gxy - gij, gwh), 1))  # box
+            anch.append(anchors[a])  # anchors
+            tcls.append(c)  # class
+
+        return tcls, tbox, indices, anch
 
 
 class PostProcess(nn.Module):
