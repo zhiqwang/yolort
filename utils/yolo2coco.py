9d75cc9f060d48b059dd9d4b8835213df62cb8d5
zhiqwang <zhiqwang@outlook.com>
2020-12-28 12:53:16 -0500

    Fix yolo-coco datasets converter

--- utils/yolo2coco.py
+++ utils/yolo2coco.py
@@ -1,9 +1,8 @@
 from pathlib import Path
-import json
+import argparse
 
-import numpy as np
+import json
 from PIL import Image
-import cv2
 
 from .builtin_meta import COCO_CATEGORIES
 
@@ -19,13 +18,13 @@ class Yolo2Coco:
         }
         self.licenses = [{
             'id': 1,
-            'name': 'Attribution-NonCommercial',
-            'url': 'http://creativecommons.org/licenses/by-nc-sa/4.0/',
+            'name': 'GNU General Public License v3.0',
+            'url': 'https://github.com/zhiqwang/yolov5-rt-stack/blob/master/LICENSE',
         }]
         self.type = 'instances'
         self.split = split
         self.root_path = Path(root)
-
+        self.label_path = self.root_path.joinpath('labels')
         self.annotation_root = self.root_path.joinpath('annotations')
         Path(self.annotation_root).mkdir(parents=True, exist_ok=True)
 
@@ -36,7 +35,7 @@ class Yolo2Coco:
         } for coco_category in COCO_CATEGORIES]
 
     def generate(self, coco_type='instances', annotation_format='bbox'):
-        label_paths = sorted(self.root_path.rglob('*.txt'))
+        label_paths = sorted(self.label_path.rglob('*.txt'))
         images, annotations = self._get_image_annotation_pairs(
             label_paths,
             annotation_format=annotation_format,
@@ -58,13 +57,13 @@ class Yolo2Coco:
         annotations = []
         annotation_id = 0
         for img_id, label_path in enumerate(label_paths, 1):
-            img_path = label_path.parents[1].joinpath('images').joinpath(f'{label_path.stem}.tif')
+            img_path = str(label_path).replace('labels', 'images').replace('txt', 'jpg')
             img = Image.open(img_path)
             width, height = img.size
 
             images.append({
                 'date_captured': '2021',
-                'file_name': str(img_path.relative_to(self.root_path)),
+                'file_name': str(Path(img_path).relative_to(self.root_path)),
                 'id': img_id,
                 'license': 1,
                 'url': '',
@@ -78,8 +77,8 @@ class Yolo2Coco:
                     assert len(label_info) == 5
                     annotation_id += 1
 
-                    label_info = [int(i) for i in label_info]
                     category_id, vertex_info = label_info[0], label_info[1:]
+                    category_id = self.categories[int(category_id)]['id']
                     if annotation_format == 'bbox':
                         segmentation, bbox, area = self._get_annotation(vertex_info, height, width)
                     else:
@@ -87,7 +86,7 @@ class Yolo2Coco:
 
                     annotations.append({
                         'segmentation': segmentation,
-                        'area': np.float(area),
+                        'area': area,
                         'iscrowd': 0,
                         'image_id': img_id,
                         'bbox': bbox,
@@ -100,11 +99,30 @@ class Yolo2Coco:
     @staticmethod
     def _get_annotation(vertex_info, height, width):
 
-        polygons = np.array(vertex_info).reshape(4, 1, -1)
-        x, y, w, h = cv2.boundingRect(polygons)
+        cx, cy, w, h = [float(i) for i in vertex_info]
+        cx = cx * width
+        cy = cy * height
+        w = w * width
+        h = h * height
+        x = cx - w / 2
+        y = cy - h / 2
 
-        segmentation = [[x, y, x + w, y, x + w, y + h, x, y + h]],
+        segmentation = [[x, y, x + w, y, x + w, y + h, x, y + h]]
+        area = w * h
 
-        assert len(polygons) > 0, "COCOAPI does not support empty polygons"
+        bbox = [x, y, w, h]
+        return segmentation, bbox, area
 
-        return segmentation, [x, y, w, h], area
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser('Datasets converter from yolo to coco', add_help=False)
+
+    parser.add_argument('--data_path', default='../coco128',
+                        help='Dataset root path')
+    parser.add_argument('--split', default='train2017',
+                        help='Dataset split part, optional: [train2017, val2017]')
+
+    args = parser.parse_args()
+
+    converter = Yolo2Coco(args.data_path, args.split)
+    converter.generate()
