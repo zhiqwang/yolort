756ef854a1a1142eb74ed8132af5c1556186b467
zhiqwang <zhiqwang@outlook.com>
2021-01-08 05:56:28 -0500

    Fix unittest of libtorch inference

--- test/tracing/trace_model.py
+++ test/tracing/trace_model.py
@@ -1,13 +1,11 @@
 import torch
 
 from hubconf import yolov5s
-from models.transform import WrappedNestedTensor
 
 
 if __name__ == "__main__":
 
     model = yolov5s(pretrained=True)
-    model = WrappedNestedTensor(model)
     model.eval()
 
     traced_model = torch.jit.script(model)
