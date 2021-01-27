40e97128e3730c3a26dc0b59cdbc702557e535ec
zhiqwang <zhiqwang@outlook.com>
2021-01-27 07:48:41 -0500

    Fixing onnx export

--- hubconf.py
+++ hubconf.py
@@ -1,8 +1,4 @@
 # Optional list of dependencies required by the package
 dependencies = ['yaml', 'torch', 'torchvision']
 
-from models import (yolov5s, yolov5m, yolov5l,
-                    yolov5_darknet_pan_s_r40,
-                    yolov5_darknet_pan_m_r40,
-                    yolov5_darknet_pan_l_r40)
-
+from models import yolov5s, yolov5m, yolov5l
--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,21 @@
+from torch import nn
+from .common import Conv
+from utils.activations import Hardswish
+
 from .yolo import (yolov5_darknet_pan_s_r31 as yolov5s,
                    yolov5_darknet_pan_m_r31 as yolov5m,
                    yolov5_darknet_pan_l_r31 as yolov5l,
                    yolov5_darknet_pan_s_r40,
                    yolov5_darknet_pan_m_r40,
                    yolov5_darknet_pan_l_r40)
+
+
+def yolov5_onnx(pretrained=False, progress=True, num_classes=80, **kwargs):
+
+    model = yolov5s(pretrained=pretrained, progress=progress, num_classes=num_classes, **kwargs)
+    for m in model.modules():
+        m._non_persistent_buffers_set = set()  # pytorch 1.6.0 compatibility
+        if isinstance(m, Conv) and isinstance(m.act, nn.Hardswish):
+            m.act = Hardswish()  # assign activation
+
+    return model
