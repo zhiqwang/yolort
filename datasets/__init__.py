49688801c022777b60202c928ba4dbd860a4eae9
zhiqwang <zhiqwang@outlook.com>
2021-02-03 22:35:00 -0500

    Rename to detection_datamodule.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,3 +1,3 @@
 # Copyright (c) 2021, Zhiqiang Wang. All Rights Reserved.
 
-from .pl_wrapper import collate_fn, DetectionDataModule
+from .detection_datamodule import collate_fn, DetectionDataModule
