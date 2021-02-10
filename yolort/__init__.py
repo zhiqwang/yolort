5ac04197437d6f1d00115166d15a91b395891151
zhiqwang <zhiqwang@outlook.com>
2021-02-10 12:59:34 -0500

    Add yolort packaging and distributing

--- yolort/__init__.py
+++ yolort/__init__.py
@@ -1,7 +1,10 @@
 # Copyright (c) 2021, Zhiqiang Wang. All Rights Reserved.
 
-__version__ = "0.3.0"
-
 from yolort import models
 from yolort import datasets
 from yolort import utils
+
+try:
+    from .version import __version__  # noqa: F401
+except ImportError:
+    pass
