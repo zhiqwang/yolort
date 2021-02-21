221b1c432e72555ef071983e7e4a9fb5917c075a
zhiqiang <zhiqwang@outlook.com>
2021-02-22 04:32:56 +0800

    Add ultralytics checkpoint updater unittest

--- test/test_utils.py
+++ test/test_utils.py
@@ -8,6 +8,10 @@ from yolort.utils import update_module_state_from_ultralytics
 
 class UtilsTester(unittest.TestCase):
     @unittest.skipIf(float(torch.__version__[:3]) > 1.7, "ultralytics release 3.1 did't support torch 1.8+")
-    def test_update_module_state_from_ultralytics_yolov5s(self):
+    def test_update_module_state_from_ultralytics_yolov5s_r31(self):
         model = update_module_state_from_ultralytics(arch='yolov5s', version='v3.1')
+        self.assertIsInstance(model, nn.Module)
+
+    def test_update_module_state_from_ultralytics_yolov5s_r40(self):
+        model = update_module_state_from_ultralytics(arch='yolov5s', version='v4.0')
         self.assertIsInstance(model, nn.Module)
