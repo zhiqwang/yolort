115f143221f082a194b316f2ab951733b368aee1
zhiqwang <zhiqwang@outlook.com>
2021-02-23 10:11:20 -0500

    Add onnx friendly institutions of nn.SiLU

--- yolort/models/__init__.py
+++ yolort/models/__init__.py
@@ -4,47 +4,58 @@ from torch import nn
 from .common import Conv
 from .yolo_module import YOLOModule
 
-from ..utils.activations import Hardswish
+from ..utils.activations import Hardswish, SiLU
 
 from typing import Any
 
 
-def yolov5s(upstream_version: str ='v3.1', **kwargs: Any):
+def yolov5s(upstream_version: str ='v3.1', export_friendly = False, **kwargs: Any):
     if upstream_version == 'v3.1':
         model = YOLOModule(arch="yolov5_darknet_pan_s_r31", **kwargs)
     elif upstream_version == 'v4.0':
         model = YOLOModule(arch="yolov5_darknet_pan_s_r40", **kwargs)
     else:
         raise NotImplementedError("Currently only supports v3.1 and v4.0 versions")
+
+    if export_friendly:
+        _export_module_friendly(model)
+
     return model
 
 
-def yolov5m(upstream_version: str ='v3.1', **kwargs: Any):
+def yolov5m(upstream_version: str ='v3.1', export_friendly: bool = False, **kwargs: Any):
     if upstream_version == 'v3.1':
         model = YOLOModule(arch="yolov5_darknet_pan_m_r31", **kwargs)
     elif upstream_version == 'v4.0':
         model = YOLOModule(arch="yolov5_darknet_pan_m_r40", **kwargs)
     else:
         raise NotImplementedError("Currently only supports v3.1 and v4.0 versions")
+
+    if export_friendly:
+        _export_module_friendly(model)
+
     return model
 
 
-def yolov5l(upstream_version: str ='v3.1', **kwargs: Any):
+def yolov5l(upstream_version: str ='v3.1', export_friendly: bool = False, **kwargs: Any):
     if upstream_version == 'v3.1':
         model = YOLOModule(arch="yolov5_darknet_pan_l_r31", **kwargs)
     elif upstream_version == 'v4.0':
         model = YOLOModule(arch="yolov5_darknet_pan_l_r40", **kwargs)
     else:
         raise NotImplementedError("Currently only supports v3.1 and v4.0 versions")
+
+    if export_friendly:
+        _export_module_friendly(model)
+
     return model
 
 
-def yolov5_onnx(pretrained=False, progress=True, num_classes=80, **kwargs):
-
-    model = yolov5s(pretrained=pretrained, progress=progress, num_classes=num_classes, **kwargs)
+def _export_module_friendly(model):
     for m in model.modules():
         m._non_persistent_buffers_set = set()  # pytorch 1.6.0 compatibility
-        if isinstance(m, Conv) and isinstance(m.act, nn.Hardswish):
-            m.act = Hardswish()  # assign activation
-
-    return model
+        if isinstance(m, Conv):
+            if isinstance(m.act, nn.Hardswish):
+                m.act = Hardswish()  # assign activation
+            if isinstance(m.act, nn.SiLU):
+                m.act = SiLU()
