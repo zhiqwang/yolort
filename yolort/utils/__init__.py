2aaee950aaf6f5731dc4b861016c49a7a8f890dd
Zhiqiang Wang <zhiqwang@foxmail.com>
2021-03-29 21:33:31 +0800

    Decoupling path dependence in notebooks (#84)
    * Cleanup parameters in Notebooks
    
    * Add image requisition from URL utils
    
    * Decoupling path dependence in notebooks
    
    * Temporarily ignore PyTorch Nightly in CI
    
    * Add some notes

--- yolort/utils/__init__.py
+++ yolort/utils/__init__.py
@@ -1,3 +1,3 @@
 from .flash_utils import get_callable_dict
-from .image_utils import cv2_imshow
+from .image_utils import cv2_imshow, get_image_from_url, read_image_to_tensor
 from .update_module_state import update_module_state_from_ultralytics
