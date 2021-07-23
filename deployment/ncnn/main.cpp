218c574b2b1cf29e16bfa53ec45ff9fe9658e7dd
Zhiqiang Wang <zhiqwang@outlook.com>
2021-07-23 06:19:08 -0400

    Fixing C++ return bug

--- deployment/ncnn/main.cpp
+++ deployment/ncnn/main.cpp
@@ -179,7 +179,7 @@ static void nms_sorted_bboxes(
   }
 }
 
-static int generate_grids_and_stride(
+static void generate_grids_and_stride(
     const int target_size,
     std::vector<int>& strides,
     std::vector<GridAndStride>& grid_strides)
