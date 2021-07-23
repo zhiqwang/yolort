389b9498edb1d6d571cab916b0d255c943952693
Zhiqiang Wang <zhiqwang@outlook.com>
2021-07-23 06:35:34 -0400

    Fixing TypeError of torch.Size

--- yolort/models/common.py
+++ yolort/models/common.py
@@ -172,7 +172,7 @@ def focus_transform(x: Tensor) -> Tensor:
 
 def space_to_depth(x: Tensor) -> Tensor:
     '''x(b,c,w,h) -> y(b,4c,w/2,h/2)'''
-    N, C, H, W = x.shape()
+    N, C, H, W = x.size()
     x = x.reshape(N, C, H // 2, 2, W // 2, 2)
     x = x.permute(0, 5, 3, 1, 2, 4)
     y = x.reshape(N, C*4, H // 2, W // 2)
