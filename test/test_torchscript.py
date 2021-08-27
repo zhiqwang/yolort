76096edb32b71981552cc68e61b1e0026d2c74ff
Zhiqiang Wang <zhiqwang@foxmail.com>
2021-08-27 22:44:44 +0800

    Simplify torchscript test and port unit-test to pytest (#152)
    * Simplify torchscript unit-test
    
    * Port test/test_*_utils.py to pytest

--- test/test_torchscript.py
+++ test/test_torchscript.py
@@ -1,11 +1,13 @@
 # Copyright (c) 2020, Zhiqiang Wang. All Rights Reserved.
+import pytest
 import torch
 
-from yolort.models import yolov5s, yolov5m, yolov5l, yolotr
+from yolort import models
 
 
-def test_yolov5s_script():
-    model = yolov5s(pretrained=True, size=(320, 320), score_thresh=0.45)
+@pytest.mark.parametrize('arch', ['yolov5s', 'yolov5m', 'yolov5l', 'yolotr'])
+def test_yolov5s_script(arch):
+    model = models.__dict__[arch](pretrained=True, size=(320, 320), score_thresh=0.45)
     model.eval()
 
     scripted_model = torch.jit.script(model)
@@ -16,54 +18,6 @@ def test_yolov5s_script():
     out = model(x)
     out_script = scripted_model(x)
 
-    torch.testing.assert_allclose(out[0]["scores"], out_script[1][0]["scores"], rtol=0., atol=0.)
-    torch.testing.assert_allclose(out[0]["labels"], out_script[1][0]["labels"], rtol=0., atol=0.)
-    torch.testing.assert_allclose(out[0]["boxes"], out_script[1][0]["boxes"], rtol=0., atol=0.)
-
-
-def test_yolov5m_script():
-    model = yolov5m(pretrained=True, size=(320, 320), score_thresh=0.45)
-    model.eval()
-
-    scripted_model = torch.jit.script(model)
-    scripted_model.eval()
-
-    x = [torch.rand(3, 288, 320), torch.rand(3, 300, 256)]
-
-    out = model(x)
-    out_script = scripted_model(x)
-    torch.testing.assert_allclose(out[0]["scores"], out_script[1][0]["scores"], rtol=0., atol=0.)
-    torch.testing.assert_allclose(out[0]["labels"], out_script[1][0]["labels"], rtol=0., atol=0.)
-    torch.testing.assert_allclose(out[0]["boxes"], out_script[1][0]["boxes"], rtol=0., atol=0.)
-
-
-def test_yolov5l_script():
-    model = yolov5l(pretrained=True, size=(320, 320), score_thresh=0.45)
-    model.eval()
-
-    scripted_model = torch.jit.script(model)
-    scripted_model.eval()
-
-    x = [torch.rand(3, 288, 320), torch.rand(3, 300, 256)]
-
-    out = model(x)
-    out_script = scripted_model(x)
-    torch.testing.assert_allclose(out[0]["scores"], out_script[1][0]["scores"], rtol=0., atol=0.)
-    torch.testing.assert_allclose(out[0]["labels"], out_script[1][0]["labels"], rtol=0., atol=0.)
-    torch.testing.assert_allclose(out[0]["boxes"], out_script[1][0]["boxes"], rtol=0., atol=0.)
-
-
-def test_yolotr_script():
-    model = yolotr(pretrained=True, size=(320, 320), score_thresh=0.45)
-    model.eval()
-
-    scripted_model = torch.jit.script(model)
-    scripted_model.eval()
-
-    x = [torch.rand(3, 288, 320), torch.rand(3, 300, 256)]
-
-    out = model(x)
-    out_script = scripted_model(x)
     torch.testing.assert_allclose(out[0]["scores"], out_script[1][0]["scores"], rtol=0., atol=0.)
     torch.testing.assert_allclose(out[0]["labels"], out_script[1][0]["labels"], rtol=0., atol=0.)
     torch.testing.assert_allclose(out[0]["boxes"], out_script[1][0]["boxes"], rtol=0., atol=0.)
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,5 +1,4 @@
 # Copyright (c) 2021, Zhiqiang Wang. All Rights Reserved.
-import unittest
 import numpy as np
 
 from torch import nn, Tensor
@@ -11,27 +10,28 @@ from yolort.utils import (
 )
 
 
-class UtilsTester(unittest.TestCase):
-    def test_update_module_state_from_ultralytics(self):
-        model = update_module_state_from_ultralytics(
-            arch='yolov5s',
-            version='v4.0',
-            feature_fusion_type='PAN',
-            num_classes=80,
-            custom_path_or_model=None,
-        )
-        self.assertIsInstance(model, nn.Module)
+def test_update_module_state_from_ultralytics():
+    model = update_module_state_from_ultralytics(
+        arch='yolov5s',
+        version='v4.0',
+        feature_fusion_type='PAN',
+        num_classes=80,
+        custom_path_or_model=None,
+    )
+    assert isinstance(model, nn.Module)
 
-    def test_read_image_to_tensor(self):
-        N, H, W = 3, 720, 360
-        img = np.random.randint(0, 255, (H, W, N), dtype='uint8')  # As a dummy image
-        out = read_image_to_tensor(img)
 
-        self.assertIsInstance(out, Tensor)
-        self.assertEqual(tuple(out.shape), (N, H, W))
+def test_read_image_to_tensor():
+    N, H, W = 3, 720, 360
+    img = np.random.randint(0, 255, (H, W, N), dtype='uint8')  # As a dummy image
+    out = read_image_to_tensor(img)
 
-    def test_get_image_from_url(self):
-        url = "https://github.com/ultralytics/yolov5/raw/master/data/images/zidane.jpg"
-        img = get_image_from_url(url)
-        self.assertIsInstance(img, np.ndarray)
-        self.assertTupleEqual(img.shape, (720, 1280, 3))
+    assert isinstance(out, Tensor)
+    assert tuple(out.shape) == (N, H, W)
+
+
+def test_get_image_from_url():
+    url = "https://github.com/ultralytics/yolov5/raw/master/data/images/zidane.jpg"
+    img = get_image_from_url(url)
+    assert isinstance(img, np.ndarray)
+    assert tuple(img.shape) == (720, 1280, 3)
