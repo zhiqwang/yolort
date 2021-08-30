ec5c5a74f9a20b338deed068de3b6fb3ab06b378
Zhiqiang Wang <zhiqwang@outlook.com>
2021-08-30 03:05:11 -0400

    Register cpu target

--- yolort/__init__.py
+++ yolort/__init__.py
@@ -1,13 +1,84 @@
 # Copyright (c) 2021, Zhiqiang Wang. All Rights Reserved.
 
+from typing import Dict, Optional, Callable
+
 from yolort import models
 from yolort import data
 from yolort import utils
 from yolort import graph
 from yolort.graph import ops
 
+from .targets.cpu import build_for_cpu_execution, cpu_xgraph_optimizer, cpu_xgraph_quantizer, cpu_xgraph_compiler
+from .graph.xop_registry import XOpRegistry, xop_register_op_layout_transform, xop_register_op_transpose_transform
+from .runtime.base_runtime import BaseRuntime
+from .runtime import rt_manager
+from .target_registry import TargetRegistry, register_op_support_check
 
 try:
     from .version import __version__  # noqa: F401
 except ImportError:
     pass
+
+
+device_r = TargetRegistry()
+
+
+# RUNTIME APIs
+
+def register_rt(
+    rt_name: str,
+    rt_graph: BaseRuntime,
+    rt_ops: Dict,
+) -> None:
+    rt_manager.register_rt(rt_name, rt_graph, rt_ops)
+
+
+def register_op(
+    rt_name: str,
+    op_type: str,
+    setup_func: Callable,
+) -> None:
+    rt_manager.register_op(rt_name, op_type, setup_func)
+
+
+def register_target(
+    target: str,
+    xgraph_optimizer: Callable,
+    xgraph_quantizer: Callable,
+    xgraph_compiler: Callable,
+    xgraph_build_func: Callable,
+    xgraph_op_support_annotator: Optional[Callable] = None,
+    skip_if_exists: bool = False,
+) -> None:
+    device_r.register_target(
+        target,
+        xgraph_optimizer,
+        xgraph_quantizer,
+        xgraph_compiler,
+        xgraph_build_func,
+        xgraph_op_support_annotator=xgraph_op_support_annotator,
+        skip_if_exists=skip_if_exists,
+    )
+
+
+register_target('cpu',
+                cpu_xgraph_optimizer,
+                cpu_xgraph_quantizer,
+                cpu_xgraph_compiler,
+                build_for_cpu_execution)
+
+
+@register_op_support_check('cpu', 'All')
+def cpu_op_support_check(X, bXs, tXs):
+    """
+    Enable all operations
+    """
+    return True
+
+
+def register_op_layout_transform(xop_name):
+    return xop_register_op_layout_transform(xop_name)
+
+
+def register_op_transpose_transform(xop_name):
+    return xop_register_op_transpose_transform(xop_name)
---
+++ yolort/targets/cpu.py
@@ -0,0 +1,55 @@
+# Copyright 2020 Xilinx Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Module for building xgraph for cpu execution
+"""
+
+from yolort.graph import XGraph
+from yolort.graph.optimization.optimizers.basic_optimizer import XGraphBasicOptimizer
+
+import copy
+
+
+# TODO move functions
+def build_for_cpu_execution(xgraph: XGraph, **kwargs) -> XGraph:
+    """
+    TODO, docstring
+    """
+    return copy.deepcopy(xgraph)
+
+
+def cpu_xgraph_optimizer(xgraph, **kwargs):
+    """
+    Basic xgraph optimizer
+    """
+
+    optimizer = XGraphBasicOptimizer(xgraph)
+    optimizer.optimize()
+
+    return xgraph
+
+
+def cpu_xgraph_quantizer(xgraph, **kwargs):
+    """
+    Basic xgraph quantizer
+    """
+    return xgraph
+
+
+def cpu_xgraph_compiler(xgraph, **kwargs):
+    """
+    Basic xgraph quantizer
+    """
+    return xgraph
