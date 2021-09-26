207913f836a9f034d0c16546c488f99c9809d208
runrunrun1994 <runrunrun1994@163.com>
2021-09-26 22:50:41 +0800

    modify export_onnx.py
    Signed-off-by: runrunrun1994 <runrunrun1994@163.com>

--- tools/export_onnx.py
+++ tools/export_onnx.py
@@ -2,24 +2,39 @@
 
 import argparse
 import os
-import sys
-sys.path.append("../")
+# import sys
+# sys.path.append("../")
 
 import onnx
 import torch
+from torch import nn
 from torchvision.ops._register_onnx_ops import _onnx_opset_version
-from yolort.models import yolov5s
+import yolort
+from yolort.v5 import Conv
+from yolort.utils.activations import Hardswish, SiLU
 
 def get_parser():
     parser = argparse.ArgumentParser('export onnx CLI for yolort', add_help=True)
+
+    parser.add_argument('--arch', type=str, required=True,
+                        help='yolov5s, yolov5m, yolov5l, yolotr')
+
+    parser.add_argument('--checkpoint_path', type=str, required=True,
+                        help='The path of model path')
+    
+    parser.add_argument('--export_friendly', type=bool, default=False,
+                        help='Replace torch.nn.silu with Silu')
+
+    parser.add_argument('--num_classes', default=80, type=int,
+                        help='The number of classes')
 
     parser.add_argument('--image_size', default=640, type=int,
                         help='Image size for evaluation (default: 640)')
     
     parser.add_argument('--batch_size', default=1, type=int,
                         help='Images per gpu, the total batch size is $NGPU x batch_size')
-    parser.add_argument('--output_dir', default='.',
-                        help='Path where to save')
+    parser.add_argument('--output_path', default='./yolov5s.onnx',
+                        help='The output name of convert onnx model')
 
     parser.add_argument('--input_names', default='input', type=str,
                         help='input name of model')
@@ -47,8 +62,6 @@ def export_onnx(model, input, save_name, input_names='input',
                dynamic=True, simplify=False,
                batch_size=1, image_size=640) :
 
-    print(input_names)
-    print(type(output_names[0]))
     print('opset version: {}'.format(_onnx_opset_version))
     torch.onnx.export(
         model,
@@ -88,15 +101,29 @@ def export_onnx(model, input, save_name, input_names='input',
         onnx.save(model_sim, onnxsim_path)
         print("End of Simplified!")
 
+def export_module_friendly(model):
+    for m in model.modules():
+        m._non_persistent_buffers_set = set()  # pytorch 1.6.0 compatibility
+        if isinstance(m, Conv):
+            if isinstance(m.act, nn.Hardswish):
+                m.act = Hardswish()  # assign activation
+            if isinstance(m.act, nn.SiLU):
+                m.act = SiLU()
+
+    return model
+
 def cli_main():
     parser = get_parser()
     args = parser.parse_args()
     print('Command Line Args: {}'.format(args))
 
+    assert args.arch in ['yolov5s', 'yolov5m', 'yolov5l', 'yolotr'], "yolort not support {}".format(args.arch)
+    assert os.path.exists(args.checkpoint_path), "{} not found".format(args.checkpoint_path)
+
     image_size    = args.image_size
     batch_size    = args.batch_size
 
-    output_dir    = args.output_dir
+    output_path   = args.output_path
     input_names   = args.input_names
     output_names  = args.output_names
     opset_version = args.opset_version
@@ -104,14 +131,20 @@ def cli_main():
     dynamic       = args.dynamic
     simplify      = args.simplify
 
-    if (not os.path.isdir(output_dir)) :
-        output_dir = "."
-
-    output_path = os.path.join(output_dir, "yolov5s.onnx")
+    base_dir, base_name = os.path.split(output_path)
+    if (not os.path.exists(base_dir)) :
+        output_path = os.path.join('.', base_name)
     
     # input data
     input = torch.randn(batch_size, 3, image_size, image_size)
-    model = yolov5s(pretrained = True, export_friendly = True)
+
+    model = yolort.models.__dict__[args.arch](
+    num_classes=args.num_classes,
+    )
+    model.load_from_yolov5(args.checkpoint_path)
+
+    if (args.export_friendly) :
+        model = export_module_friendly(model)
     model.eval()
 
     # export onnx
