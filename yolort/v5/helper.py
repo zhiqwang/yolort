5421eea92492bbdf597dbf022cd96628f499e215
zhiqiang <zhiqwang@outlook.com>
2021-10-08 00:31:36 +0800

    Cleanup get_yolov5_size

--- yolort/v5/helper.py
+++ yolort/v5/helper.py
@@ -30,15 +30,14 @@ def add_yolov5_context():
 def get_yolov5_size(depth_multiple, width_multiple):
     if depth_multiple == 0.33 and width_multiple == 0.5:
         return "s"
-    elif depth_multiple == 0.67 and width_multiple == 0.75:
+    if depth_multiple == 0.67 and width_multiple == 0.75:
         return "m"
-    elif depth_multiple == 1.0 and width_multiple == 1.0:
+    if depth_multiple == 1.0 and width_multiple == 1.0:
         return "l"
-    else:
-        raise NotImplementedError(
-            f"Currently does't support architecture with depth: {depth_multiple} "
-            f"and {width_multiple}, fell free to create a ticket labeled enhancement to us"
-        )
+    raise NotImplementedError(
+        f"Currently does't support architecture with depth: {depth_multiple} "
+        f"and {width_multiple}, fell free to create a ticket labeled enhancement to us"
+    )
 
 
 def load_yolov5_model(
