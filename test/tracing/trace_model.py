b67bf2b95fd514908e55cf8bbe83eab9c31dc055
zhiqiang <zhiqwang@outlook.com>
2021-10-21 11:46:21 +0800

    Add dummy inputs for tracing

--- test/tracing/trace_model.py
+++ test/tracing/trace_model.py
@@ -7,5 +7,6 @@ if __name__ == "__main__":
     model = yolov5s(pretrained=True)
     model.eval()
 
-    traced_model = torch.jit.trace(model)
+    dummy_inputs = [torch.rand(3, 416, 352), torch.rand(3, 480, 384)]
+    traced_model = torch.jit.trace(model, dummy_inputs)
     traced_model.save("./test/tracing/yolov5s.torchscript.pt")
