fdb4a3254b554e5fac5cbb1b2591591125589be3
nihui <shuizhuyuanluo@126.com>
2021-10-21 10:09:55 +0800

    Update trace_model.py

--- test/tracing/trace_model.py
+++ test/tracing/trace_model.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     model = yolov5s(pretrained=True)
     model.eval()
 
-    traced_model = torch.jit.script(model)
+    traced_model = torch.jit.trace(model)
     traced_model.save("./test/tracing/yolov5s.torchscript.pt")
