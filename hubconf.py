ca5c7f9f0d90bd903ecad57bfe65e3b3e43747ca
zhiqiang <zhiqwang@outlook.com>
2021-10-24 09:03:03 +0800

    Add yolov5 P6 to hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,4 +1,13 @@
 # Optional list of dependencies required by the package
 dependencies = ["torch", "torchvision"]
 
-from yolort.models import yolov5n, yolov5s, yolov5m, yolov5l, yolov5ts
+from yolort.models import (
+    yolov5n,
+    yolov5n6,
+    yolov5s,
+    yolov5s6,
+    yolov5m,
+    yolov5m6,
+    yolov5l,
+    yolov5ts,
+)
