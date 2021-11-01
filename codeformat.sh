768fb77a6e600ec7b2bcb52c9329dc8e3a733950
Zhiqiang Wang <zhiqwang@foxmail.com>
2021-11-01 10:13:28 +0800

    Minor fixes

--- codeformat.sh
+++ codeformat.sh
@@ -2,10 +2,10 @@
 
 # we run clang-format and astyle twice to get stable format output
 
-find deployment/ tools/ tests/ -type f -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' | xargs -i clang-format -i {}
-astyle -n -r "tests/*.h,*.cpp,*.cc" "tools/*.h,*.cpp,*.cc"
+find deployment/ tools/ test/ -type f -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' | xargs -i clang-format -i {}
+astyle -n -r "test/*.h,*.cpp,*.cc" "tools/*.h,*.cpp,*.cc"
 astyle -n -r "deployment/*.h,*.cpp,*.cc"
 
-find deployment/ tools/ tests/ -type f -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' | xargs -i clang-format -i {}
-astyle -n -r "tests/*.h,*.cpp,*.cc" "tools/*.h,*.cpp,*.cc"
+find deployment/ tools/ test/ -type f -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' | xargs -i clang-format -i {}
+astyle -n -r "test/*.h,*.cpp,*.cc" "tools/*.h,*.cpp,*.cc"
 astyle -n -r "deployment/*.h,*.cpp,*.cc"
