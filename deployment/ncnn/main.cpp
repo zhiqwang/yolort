4cb5fb9ecaedf469cdea85504d6f3cb3fb8b6178
zhiqiang <zhiqwang@foxmail.com>
2021-11-29 13:24:27 +0800

    Abstract the AnchorGenerator and LogitsDecoder in ncnn

--- deployment/ncnn/main.cpp
+++ deployment/ncnn/main.cpp
@@ -1,20 +1,3 @@
-// Tencent is pleased to support the open source community by making ncnn
-// available.
-//
-// Copyright (C) 2020 THL A29 Limited, a Tencent company. All rights reserved.
-//
-// Licensed under the BSD 3-Clause License (the "License"); you may not use this
-// file except in compliance with the License. You may obtain a copy of the
-// License at
-//
-// https://opensource.org/licenses/BSD-3-Clause
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations under
-// the License.
-
 #include "layer.h"
 #include "net.h"
 
@@ -29,49 +12,36 @@
 #include <stdio.h>
 #include <vector>
 
-class YOLOv5Focus : public ncnn::Layer {
+DEFINE_LAYER_CREATOR(LetterBox)
+
+class AnchorGenerator : public ncnn::Layer {
  public:
-  YOLOv5Focus() {
+  AnchorGenerator() {
     one_blob_only = true;
   }
 
   virtual int forward(const ncnn::Mat& bottom_blob, ncnn::Mat& top_blob, const ncnn::Option& opt)
       const {
-    int w = bottom_blob.w;
-    int h = bottom_blob.h;
-    int channels = bottom_blob.c;
-
-    int outw = w / 2;
-    int outh = h / 2;
-    int outc = channels * 4;
-
-    top_blob.create(outw, outh, outc, 4u, 1, opt.blob_allocator);
-    if (top_blob.empty())
-      return -100;
-
-#pragma omp parallel for num_threads(opt.num_threads)
-    for (int p = 0; p < outc; p++) {
-      const float* ptr =
-          bottom_blob.channel(p % channels).row((p / channels) % 2) + ((p / channels) / 2);
-      float* outptr = top_blob.channel(p);
-
-      for (int i = 0; i < outh; i++) {
-        for (int j = 0; j < outw; j++) {
-          *outptr = *ptr;
-
-          outptr += 1;
-          ptr += 2;
-        }
-
-        ptr += w;
-      }
-    }
-
     return 0;
   }
 };
 
-DEFINE_LAYER_CREATOR(YOLOv5Focus)
+DEFINE_LAYER_CREATOR(AnchorGenerator)
+
+
+class LogitsDecoder : public ncnn::Layer {
+ public:
+  LogitsDecoder() {
+    one_blob_only = true;
+  }
+
+  virtual int forward(const ncnn::Mat& bottom_blob, ncnn::Mat& top_blob, const ncnn::Option& opt)
+      const {
+    return 0;
+  }
+};
+
+DEFINE_LAYER_CREATOR(LogitsDecoder)
 
 struct Object {
   cv::Rect_<float> rect;
@@ -130,7 +100,7 @@ static void qsort_descent_inplace(std::vector<Object>& objects) {
 static void nms_sorted_bboxes(
     const std::vector<Object>& faceobjects,
     std::vector<int>& picked,
-    float nms_threshold) {
+    float nms_thresh) {
   picked.clear();
 
   const int n = faceobjects.size();
@@ -151,7 +121,7 @@ static void nms_sorted_bboxes(
       float inter_area = intersection_area(a, b);
       float union_area = areas[i] + areas[picked[j]] - inter_area;
       // float IoU = inter_area / union_area
-      if (inter_area / union_area > nms_threshold)
+      if (inter_area / union_area > nms_thresh)
         keep = 0;
     }
 
@@ -164,108 +134,21 @@ static inline float sigmoid(float x) {
   return static_cast<float>(1.f / (1.f + exp(-x)));
 }
 
-static void generate_proposals(
-    const ncnn::Mat& anchors,
-    int stride,
-    const ncnn::Mat& in_pad,
-    const ncnn::Mat& feat_blob,
-    float prob_threshold,
-    std::vector<Object>& objects) {
-  const int num_grid = feat_blob.h;
+static int detect(const cv::Mat& bgr, std::vector<Object>& objects) {
+  ncnn::Net yolort;
 
-  int num_grid_x;
-  int num_grid_y;
-  if (in_pad.w > in_pad.h) {
-    num_grid_x = in_pad.w / stride;
-    num_grid_y = num_grid / num_grid_x;
-  } else {
-    num_grid_y = in_pad.h / stride;
-    num_grid_x = num_grid / num_grid_y;
-  }
+  // yolort.opt.use_vulkan_compute = true;
+  // yolort.opt.use_bf16_storage = true;
 
-  const int num_class = feat_blob.w - 5;
+  yolort.register_custom_layer("AnchorGenerator", AnchorGenerator_layer_creator);
+  yolort.register_custom_layer("LogitsDecoder", LogitsDecoder_layer_creator);
 
-  const int num_anchors = anchors.w / 2;
-
-  for (int q = 0; q < num_anchors; q++) {
-    const float anchor_w = anchors[q * 2];
-    const float anchor_h = anchors[q * 2 + 1];
-
-    const ncnn::Mat feat = feat_blob.channel(q);
-
-    for (int i = 0; i < num_grid_y; i++) {
-      for (int j = 0; j < num_grid_x; j++) {
-        const float* featptr = feat.row(i * num_grid_x + j);
-
-        // find class index with max class score
-        int class_index = 0;
-        float class_score = -FLT_MAX;
-        for (int k = 0; k < num_class; k++) {
-          float score = featptr[5 + k];
-          if (score > class_score) {
-            class_index = k;
-            class_score = score;
-          }
-        }
-
-        float box_score = featptr[4];
-
-        float confidence = sigmoid(box_score) * sigmoid(class_score);
-
-        if (confidence >= prob_threshold) {
-          // yolov5/models/yolo.py Detect forward
-          // y = x[i].sigmoid()
-          // y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 +
-          // self.grid[i].to(x[i].device)) * self.stride[i]  # xy y[..., 2:4] =
-          // (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
-
-          float dx = sigmoid(featptr[0]);
-          float dy = sigmoid(featptr[1]);
-          float dw = sigmoid(featptr[2]);
-          float dh = sigmoid(featptr[3]);
-
-          float pb_cx = (dx * 2.f - 0.5f + j) * stride;
-          float pb_cy = (dy * 2.f - 0.5f + i) * stride;
-
-          float pb_w = pow(dw * 2.f, 2) * anchor_w;
-          float pb_h = pow(dh * 2.f, 2) * anchor_h;
-
-          float x0 = pb_cx - pb_w * 0.5f;
-          float y0 = pb_cy - pb_h * 0.5f;
-          float x1 = pb_cx + pb_w * 0.5f;
-          float y1 = pb_cy + pb_h * 0.5f;
-
-          Object obj;
-          obj.rect.x = x0;
-          obj.rect.y = y0;
-          obj.rect.width = x1 - x0;
-          obj.rect.height = y1 - y0;
-          obj.label = class_index;
-          obj.prob = confidence;
-
-          objects.push_back(obj);
-        }
-      }
-    }
-  }
-}
-
-static int detect_yolov5(const cv::Mat& bgr, std::vector<Object>& objects) {
-  ncnn::Net yolov5;
-
-  yolov5.opt.use_vulkan_compute = true;
-  // yolov5.opt.use_bf16_storage = true;
-
-  yolov5.register_custom_layer("YOLOv5Focus", YOLOv5Focus_layer_creator);
-
-  // original pretrained model from https://github.com/ultralytics/yolov5
-  // the ncnn model https://github.com/nihui/ncnn-assets/tree/master/models
-  yolov5.load_param("yolort-opt.param");
-  yolov5.load_model("yolort-opt.bin");
+  yolort.load_param("yolort-opt.param");
+  yolort.load_model("yolort-opt.bin");
 
   const int target_size = 640;
-  const float prob_threshold = 0.25f;
-  const float nms_threshold = 0.45f;
+  const float score_thresh = 0.25f;
+  const float nms_thresh = 0.45f;
 
   int img_w = bgr.cols;
   int img_h = bgr.rows;
@@ -305,83 +188,21 @@ static int detect_yolov5(const cv::Mat& bgr, std::vector<Object>& objects) {
   const float norm_vals[3] = {1 / 255.f, 1 / 255.f, 1 / 255.f};
   in_pad.substract_mean_normalize(0, norm_vals);
 
-  ncnn::Extractor ex = yolov5.create_extractor();
+  ncnn::Extractor ex = yolort.create_extractor();
 
   ex.input("images", in_pad);
 
-  std::vector<Object> proposals;
+  std::vector<Object> anchors;
 
-  // anchor setting from yolov5/models/yolov5s.yaml
-
-  // stride 8
-  {
-    ncnn::Mat out;
-    ex.extract("h1", out);
-
-    ncnn::Mat anchors(6);
-    anchors[0] = 10.f;
-    anchors[1] = 13.f;
-    anchors[2] = 16.f;
-    anchors[3] = 30.f;
-    anchors[4] = 33.f;
-    anchors[5] = 23.f;
-
-    std::vector<Object> objects8;
-    generate_proposals(anchors, 8, in_pad, out, prob_threshold, objects8);
-
-    proposals.insert(proposals.end(), objects8.begin(), objects8.end());
-  }
-
-  // stride 16
-  {
-    ncnn::Mat out;
-    ex.extract("h2", out);
-
-    ncnn::Mat anchors(6);
-    anchors[0] = 30.f;
-    anchors[1] = 61.f;
-    anchors[2] = 62.f;
-    anchors[3] = 45.f;
-    anchors[4] = 59.f;
-    anchors[5] = 119.f;
-
-    std::vector<Object> objects16;
-    generate_proposals(anchors, 16, in_pad, out, prob_threshold, objects16);
-
-    proposals.insert(proposals.end(), objects16.begin(), objects16.end());
-  }
-
-  // stride 32
-  {
-    ncnn::Mat out;
-    ex.extract("h3", out);
-
-    ncnn::Mat anchors(6);
-    anchors[0] = 116.f;
-    anchors[1] = 90.f;
-    anchors[2] = 156.f;
-    anchors[3] = 198.f;
-    anchors[4] = 373.f;
-    anchors[5] = 326.f;
-
-    std::vector<Object> objects32;
-    generate_proposals(anchors, 32, in_pad, out, prob_threshold, objects32);
-
-    proposals.insert(proposals.end(), objects32.begin(), objects32.end());
-  }
-
-  // sort all proposals by score from highest to lowest
-  qsort_descent_inplace(proposals);
-
-  // apply nms with nms_threshold
+  // apply nms with nms_thresh
   std::vector<int> picked;
-  nms_sorted_bboxes(proposals, picked, nms_threshold);
+  nms_sorted_bboxes(anchors, picked, nms_thresh);
 
   int count = picked.size();
 
   objects.resize(count);
   for (int i = 0; i < count; i++) {
-    objects[i] = proposals[picked[i]];
+    objects[i] = anchors[picked[i]];
 
     // adjust offset to original unpadded
     float x0 = (objects[i].rect.x - (wpad / 2)) / scale;
@@ -404,99 +225,43 @@ static int detect_yolov5(const cv::Mat& bgr, std::vector<Object>& objects) {
   return 0;
 }
 
-static void draw_objects(const cv::Mat& bgr, const std::vector<Object>& objects) {
-  static const char* class_names[] = {"person",        "bicycle",      "car",
-                                      "motorcycle",    "airplane",     "bus",
-                                      "train",         "truck",        "boat",
-                                      "traffic light", "fire hydrant", "stop sign",
-                                      "parking meter", "bench",        "bird",
-                                      "cat",           "dog",          "horse",
-                                      "sheep",         "cow",          "elephant",
-                                      "bear",          "zebra",        "giraffe",
-                                      "backpack",      "umbrella",     "handbag",
-                                      "tie",           "suitcase",     "frisbee",
-                                      "skis",          "snowboard",    "sports ball",
-                                      "kite",          "baseball bat", "baseball glove",
-                                      "skateboard",    "surfboard",    "tennis racket",
-                                      "bottle",        "wine glass",   "cup",
-                                      "fork",          "knife",        "spoon",
-                                      "bowl",          "banana",       "apple",
-                                      "sandwich",      "orange",       "broccoli",
-                                      "carrot",        "hot dog",      "pizza",
-                                      "donut",         "cake",         "chair",
-                                      "couch",         "potted plant", "bed",
-                                      "dining table",  "toilet",       "tv",
-                                      "laptop",        "mouse",        "remote",
-                                      "keyboard",      "cell phone",   "microwave",
-                                      "oven",          "toaster",      "sink",
-                                      "refrigerator",  "book",         "clock",
-                                      "vase",          "scissors",     "teddy bear",
-                                      "hair drier",    "toothbrush"};
+int main(int argc, char* argv[]) {
+  cmdline::parser cmd;
+  cmd.add<std::string>("model_path", 'm', "Path to ncnn model.", true, "yolov5.param");
+  cmd.add<std::string>("image", 'i', "Image source to be detected.", true, "bus.jpg");
+  cmd.add<std::string>("class_names", 'c', "Path of dataset labels.", true, "coco.names");
+  cmd.add("gpu", '\0', "Enable cuda device or cpu.");
 
-  cv::Mat image = bgr.clone();
+  cmd.parse_check(argc, argv);
 
-  for (size_t i = 0; i < objects.size(); i++) {
-    const Object& obj = objects[i];
+  bool isGPU = cmd.exist("gpu");
+  std::string classNamesPath = cmd.get<std::string>("class_names");
+  std::vector<std::string> classNames = utils::loadNames(classNamesPath);
+  std::string imagePath = cmd.get<std::string>("image");
+  std::string modelPath = cmd.get<std::string>("model_path");
 
-    fprintf(
-        stderr,
-        "%d = %.5f at %.2f %.2f %.2f x %.2f\n",
-        obj.label,
-        obj.prob,
-        obj.rect.x,
-        obj.rect.y,
-        obj.rect.width,
-        obj.rect.height);
-
-    cv::rectangle(image, obj.rect, cv::Scalar(255, 0, 0));
-
-    char text[256];
-    sprintf(text, "%s %.1f%%", class_names[obj.label], obj.prob * 100);
-
-    int baseLine = 0;
-    cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
-
-    int x = obj.rect.x;
-    int y = obj.rect.y - label_size.height - baseLine;
-    if (y < 0)
-      y = 0;
-    if (x + label_size.width > image.cols)
-      x = image.cols - label_size.width;
-
-    cv::rectangle(
-        image,
-        cv::Rect(cv::Point(x, y), cv::Size(label_size.width, label_size.height + baseLine)),
-        cv::Scalar(255, 255, 255),
-        -1);
-
-    cv::putText(
-        image,
-        text,
-        cv::Point(x, y + label_size.height),
-        cv::FONT_HERSHEY_SIMPLEX,
-        0.5,
-        cv::Scalar(0, 0, 0));
-  }
-}
-
-int main(int argc, char** argv) {
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s [imagepath]\n", argv[0]);
+  if (classNames.empty()) {
+    std::cout << "Empty class names file." << std::endl;
     return -1;
   }
 
-  const char* imagepath = argv[1];
-
-  cv::Mat m = cv::imread(imagepath, 1);
-  if (m.empty()) {
-    fprintf(stderr, "cv::imread %s failed\n", imagepath);
+  YOLOv5Detector detector{nullptr};
+  try {
+    detector = YOLOv5Detector(modelPath, isGPU);
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
     return -1;
   }
 
-  std::vector<Object> objects;
-  detect_yolov5(m, objects);
+  cv::Mat image = cv::imread(imagePath);
 
-  draw_objects(m, objects);
+  std::vector<Detection> result = detector.detect(image);
+
+  utils::visualizeDetection(image, result, classNames);
+
+  cv::imshow("result", image);
+  // cv::imwrite("result.jpg", image);
+  cv::waitKey(0);
 
   return 0;
 }
