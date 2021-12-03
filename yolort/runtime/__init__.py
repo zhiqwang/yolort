770e57316d5ed4656bed22768caa501c09c38218
Zhiqiang Wang <zhiqwang@foxmail.com>
2021-12-04 00:47:44 +0800

    Move ort_modeling.py to y_onnxruntime.py

--- yolort/runtime/__init__.py
+++ yolort/runtime/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2021, yolort team. All Rights Reserved.
-from .ort_modeling import PredictorORT
+from .y_onnxruntime import PredictorORT
 from .yolo_tensorrt_model import YOLOTRTModule
 
 __all__ = ["PredictorORT", "YOLOTRTModule"]
