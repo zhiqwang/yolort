e9fb0eb5d2a4bdbe95864f9b6f252d847691b9b2
Zhiqiang Wang <zhiqwang@foxmail.com>
2021-12-05 17:52:04 +0800

    Add test_trt_model_onnx_saves

--- test/test_runtime.py
+++ test/test_runtime.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2021, yolort team. All Rights Reserved.
+from pathlib import Path
 import pytest
 import torch
 from torch import Tensor
@@ -34,3 +35,29 @@ def test_yolo_trt_module(arch, version, upstream_version, hash_prefix):
     assert isinstance(outs[0], dict)
     assert isinstance(outs[0]["boxes"], Tensor)
     assert isinstance(outs[0]["scores"], Tensor)
+
+
+@pytest.mark.parametrize(
+    "arch, version, upstream_version, hash_prefix",
+    [
+        ("yolov5s", "r4.0", "v4.0", "9ca9a642"),
+        ("yolov5n", "r6.0", "v6.0", "649e089f"),
+        ("yolov5s", "r6.0", "v6.0", "c3b140f3"),
+        ("yolov5n6", "r6.0", "v6.0", "beecbbae"),
+    ],
+)
+def test_trt_model_onnx_saves(arch, version, upstream_version, hash_prefix):
+    base_url = "https://github.com/ultralytics/yolov5/releases/download/"
+    model_url = f"{base_url}/{upstream_version}/{arch}.pt"
+    checkpoint_path = attempt_download(model_url, hash_prefix=hash_prefix)
+    score_thresh = 0.25
+
+    model = YOLOTRTModule(
+        checkpoint_path,
+        score_thresh=score_thresh,
+        version=version,
+    )
+    model.eval()
+    onnx_file_path = f"trt_model_onnx_saves_{arch}_{hash_prefix}.onnx"
+    model.to_onnx(onnx_file_path)
+    assert Path(onnx_file_path).exists()
