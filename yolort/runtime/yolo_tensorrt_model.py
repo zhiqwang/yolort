e2ecf5777720228815dbbd954fc22253818df9cf
Zhiqiang Wang <zhiqwang@foxmail.com>
2021-12-05 16:57:54 +0800

    Add to_onnx method in YOLOTRTModule

--- yolort/runtime/yolo_tensorrt_model.py
+++ yolort/runtime/yolo_tensorrt_model.py
@@ -1,14 +1,15 @@
 # Copyright (c) 2021, yolort team. All Rights Reserved.
 import torch
-from pytorch_lightning import LightningModule
-from torch import Tensor
+from pathlib import Path
+from torch import nn, Tensor
+from typing import Dict, List, Optional, Union
 from yolort.models import YOLO
 from yolort.models.box_head import LogitsDecoder
 
 __all__ = ["YOLOTRTModule"]
 
 
-class YOLOTRTModule(LightningModule):
+class YOLOTRTModule(nn.Module):
     """
     TensorRT deployment friendly wrapper for YOLO.
 
@@ -32,7 +33,7 @@ class YOLOTRTModule(LightningModule):
         )
 
     @torch.no_grad()
-    def forward(self, inputs: Tensor):
+    def forward(self, inputs: Tensor) -> List[Dict[str, Tensor]]:
         """
         Args:
             inputs (Tensor): batched images, of shape [batch_size x 3 x H x W]
@@ -41,3 +42,46 @@ class YOLOTRTModule(LightningModule):
         outputs = self.model(inputs)
 
         return outputs
+
+    @torch.no_grad()
+    def to_onnx(
+        self,
+        file_path: Union[str, Path],
+        input_sample: Optional[Tensor] = None,
+        opset_version: int = 11,
+        enable_dynamic: bool = True,
+        **kwargs,
+    ):
+        """
+        Saves the model in ONNX format.
+
+        Args:
+            file_path: The path of the file the onnx model should be saved to.
+            input_sample: An input for tracing. Default: None.
+            opset_version: Opset version we export the model to the onnx submodule. Default: 11.
+            enable_dynamic: Whether to specify axes of tensors as dynamic. Default: True.
+            **kwargs: Will be passed to torch.onnx.export function.
+        """
+        if input_sample is None:
+            input_sample = torch.rand(1, 3, 320, 320)
+
+        dynamic_axes = {
+            "images_tensors": {0: "batch", 2: "height", 3: "width"},
+            "boxes": {0: "batch", 1: "num_objects"},
+            "scores": {0: "batch", 1: "num_objects"},
+        } if enable_dynamic else None
+
+        input_names = ["images_tensors"]
+        output_names = ["boxes", "scores"]
+
+        torch.onnx.export(
+            self.model,
+            input_sample,
+            file_path,
+            do_constant_folding=True,
+            opset_version=opset_version,
+            input_names=input_names,
+            output_names=output_names,
+            dynamic_axes=dynamic_axes,
+            **kwargs,
+        )
