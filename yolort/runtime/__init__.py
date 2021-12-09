e382738dac895783d1c052e9c1d6ec2e13bc8317
Zhiqiang Wang <zhiqwang@foxmail.com>
2021-12-09 23:28:23 +0800

    Fix importing yolort.runtime

--- yolort/runtime/__init__.py
+++ yolort/runtime/__init__.py
@@ -1,6 +1,5 @@
 # Copyright (c) 2021, yolort team. All Rights Reserved.
 from .y_onnxruntime import PredictorORT
-from .y_tensorrt import PredictorTRT
 from .yolo_tensorrt_model import YOLOTRTModule
 
-__all__ = ["PredictorORT", "PredictorTRT", "YOLOTRTModule"]
+__all__ = ["PredictorORT", "YOLOTRTModule"]
