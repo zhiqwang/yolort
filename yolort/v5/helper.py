0934555efa44222900845437434502b51247a1b7
shiquanyu <crashdown@foxmail.com>
2021-12-29 19:50:04 +0800

    add config for yolov5-x

--- yolort/v5/helper.py
+++ yolort/v5/helper.py
@@ -37,6 +37,8 @@ def get_yolov5_size(depth_multiple, width_multiple):
         return "m"
     if depth_multiple == 1.0 and width_multiple == 1.0:
         return "l"
+    if depth_multiple == 1.33 and width_multiple == 1.25:
+        return "x"
     raise NotImplementedError(
         f"Currently does't support architecture with depth: {depth_multiple} "
         f"and {width_multiple}, fell free to create a ticket labeled enhancement to us"
