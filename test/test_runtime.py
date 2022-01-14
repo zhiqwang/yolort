7bd40e36fa20260568c6f986fd6c91ef8b028f7c
Zhiqiang Wang <zhiqwang@foxmail.com>
2022-01-14 13:24:55 +0800

    Add unittest for yolo_graphsurgeon_register_nms

--- test/test_runtime.py
+++ test/test_runtime.py
@@ -76,3 +76,25 @@ def test_yolo_graphsurgeon_wo_nms(arch, version, upstream_version, hash_prefix):
     assert not Path(onnx_file_path).exists()
     yolo_gs.save(onnx_file_path)
     assert Path(onnx_file_path).exists()
+
+
+@pytest.mark.parametrize(
+    "arch, version, upstream_version, hash_prefix",
+    [
+        ("yolov5s", "r4.0", "v4.0", "9ca9a642"),
+        ("yolov5n", "r6.0", "v6.0", "649e089f"),
+        ("yolov5s", "r6.0", "v6.0", "c3b140f3"),
+        ("yolov5n6", "r6.0", "v6.0", "beecbbae"),
+    ],
+)
+def test_yolo_graphsurgeon_register_nms(arch, version, upstream_version, hash_prefix):
+    base_url = "https://github.com/ultralytics/yolov5/releases/download/"
+    model_url = f"{base_url}/{upstream_version}/{arch}.pt"
+    checkpoint_path = attempt_download(model_url, hash_prefix=hash_prefix)
+
+    yolo_gs = YOLOGraphSurgeon(checkpoint_path, version=version, enable_dynamic=False)
+    yolo_gs.register_nms()
+    onnx_file_path = f"yolo_graphsurgeon_register_nms{arch}_{hash_prefix}.onnx"
+    assert not Path(onnx_file_path).exists()
+    yolo_gs.save(onnx_file_path)
+    assert Path(onnx_file_path).exists()
