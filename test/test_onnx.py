f1626c25df4e69587bbba3854a8f97386c39bc9e
Zhiqiang Wang <zhiqwang@foxmail.com>
2022-01-20 00:59:53 +0800

    Enable `yolov5ts` unittest for ONNX exporting

--- test/test_onnx.py
+++ test/test_onnx.py
@@ -110,7 +110,7 @@ class TestONNXExporter:
         [
             ("yolov5s", "r3.1"),
             ("yolov5m", "r4.0"),
-            # ("yolov5ts", "r4.0"),
+            ("yolov5ts", "r4.0"),
         ],
     )
     def test_yolort_export_onnx(self, arch, upstream_version):
