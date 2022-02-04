b95e3574ef97811bba8c1feea163c79ed114f7d5
Zhiqiang Wang <zhiqwang@foxmail.com>
2022-02-04 14:29:32 +0800

    Fixing YOLOTransform letterbox batching

--- test/test_models_transform.py
+++ test/test_models_transform.py
@@ -26,22 +26,27 @@ def test_yolo_transform():
 
 @pytest.mark.parametrize("img_h", [300, 500, 720, 800, 1080, 1280])
 @pytest.mark.parametrize("img_w", [300, 500, 720, 800, 1080, 1280])
-@pytest.mark.parametrize("auto", [True, False])
+@pytest.mark.parametrize("auto", [True])
 @pytest.mark.parametrize("stride", [32, 64])
 def test_letterbox(img_h, img_w, auto, stride):
-    from yolort.models.transform import _letterbox as letterbox1
-    from yolort.v5 import letterbox as letterbox2
+
+    from yolort.models.transform import _resize_image_and_masks
+    from yolort.v5 import letterbox
+
+    new_shape = (640, 640)  # height, width
 
     img_tensor = torch.randint(0, 255, (3, img_h, img_w))
     img_numpy = img_tensor.permute(1, 2, 0).numpy().astype("uint8")
 
-    out1 = letterbox1(img_tensor, auto=auto, stride=stride)
-    out2 = letterbox2(img_numpy, auto=auto, stride=stride)
+    yolo_transform = YOLOTransform(new_shape[0], new_shape[1], size_divisible=stride, auto_rectangle=auto)
 
-    assert out1[1] == out2[1]
-    assert out1[2] == out2[2]
+    im3 = img_tensor / 255
+    im3, _ = _resize_image_and_masks(im3.float(), new_shape)
+    out1 = yolo_transform.batch_images([im3])
 
-    aug1 = out1[0][0].numpy()
+    out2 = letterbox(img_numpy, new_shape=new_shape, auto=auto, stride=stride)
+
+    aug1 = out1[0].numpy()
     aug2 = out2[0].astype(np.float32)  # uint8 to float32
     aug2 = np.transpose(aug2 / 255.0, [2, 0, 1])
     assert aug1.shape == aug2.shape
