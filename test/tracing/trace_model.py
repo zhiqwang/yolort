0c159a7926aa0a06b22f21c4ee8c55fbf33f7ccd
yang-gis <yangjinyi_gis@163.com>
2022-02-11 14:41:51 +0800

    batch_infer

--- test/tracing/trace_model.py
+++ test/tracing/trace_model.py
@@ -1,10 +1,37 @@
-from yolort.models import yolov5s
+"""
+detect from U版 yolov5
+"""
+
+import os
+import torch
+#from yolort.models import YOLO
+from yolort.models import YOLOv5
 from yolort.relaying.trace_wrapper import get_trace_module
 
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-if __name__ == "__main__":
+score_thresh = 0.35
+iou = 0.45
+checkpoint_path = "./model_file/best_0128.pt"
 
-    model = yolov5s(pretrained=True)
-    traced_model = get_trace_module(model, input_shape=(416, 352))
 
-    traced_model.save("./test/tracing/yolov5s.torchscript.pt")
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+
+# YOLO.load_from_yolov5之后后处理nms，无前处理
+# YOLOv5.load_from_yolov5已做前处理，并与U版做了精度对齐
+
+model_yolort = YOLOv5.load_from_yolov5(
+    checkpoint_path,
+    score_thresh=score_thresh
+)
+
+model_yolort = model_yolort.to(device)
+model_yolort = model_yolort.eval()
+
+traced_model = get_trace_module(model_yolort, input_shape=(576, 2016))
+
+traced_model.save("best_traced_nopre.pt")
