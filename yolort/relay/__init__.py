c55711d74d0e6d2d97081b5e5ac674996c7c8b49
Zhiqiang Wang <zhiqwang@foxmail.com>
2022-02-13 12:30:11 +0800

    Rename yolo_inference to trt_inference.py

--- yolort/relay/__init__.py
+++ yolort/relay/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2021, yolort team. All rights reserved.
 
 from .trace_wrapper import get_trace_module
-from .yolo_inference import YOLOInference
+from .trt_inference import YOLOInference
 
 __all__ = ["get_trace_module", "YOLOInference"]
