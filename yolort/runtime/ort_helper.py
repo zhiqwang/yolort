0c7d28234bc1a74e69d9c203489ea2084e75b212
Zhiqiang Wang <zhiqwang@foxmail.com>
2022-02-15 16:23:36 +0800

    Fixing export_onnx function

--- yolort/runtime/ort_helper.py
+++ yolort/runtime/ort_helper.py
@@ -1,19 +1,21 @@
 # Copyright (c) 2022, yolort team. All rights reserved.
 
-from pathlib import Path
-from typing import Optional
+from typing import Optional, Tuple
 
 import torch
+from torch import nn
 from yolort.models import YOLO, YOLOv5
 
 
 def export_onnx(
-    checkpoint_path,
+    onnx_path: str,
+    checkpoint_path: Optional[str] = None,
+    model: Optional[nn.Module] = None,
+    size: Tuple[int, int] = (640, 640),
+    size_divisible: int = 32,
     score_thresh: float = 0.25,
     nms_thresh: float = 0.45,
     version: str = "r6.0",
-    onnx_path: Optional[str] = None,
-    batch_size: int = 1,
     skip_preprocess: bool = False,
     opset_version: int = 11,
 ) -> None:
@@ -21,22 +23,30 @@ def export_onnx(
     Export to ONNX models that can be used for ONNX Runtime inferencing.
 
     Args:
-        checkpoint_path (string): Path of the custom trained YOLOv5 checkpoint.
+        onnx_path (string): The path to the ONNX graph to be exported.
+        checkpoint_path (string, optional): Path of the custom trained YOLOv5 checkpoint.
+            Default: None
+        model (nn.Module): The defined PyTorch module to be exported. Default: None
+        size: (Tuple[int, int]): the minimum and maximum size of the image to be rescaled.
+            Default: (640, 640)
+        size_divisible (int): stride of the models. Default: 32
         score_thresh (float): Score threshold used for postprocessing the detections.
             Default: 0.25
         nms_thresh (float): NMS threshold used for postprocessing the detections. Default: 0.45
         version (string): Upstream YOLOv5 version. Default: 'r6.0'
-        batch_size (int): Batch size for exporting YOLOv5.
         skip_preprocess (bool): Skip the preprocessing transformation when exporting the ONNX
             models. Default: False
         opset_version (int): Opset version for exporting ONNX models. Default: 11
     """
+
     onnx_builder = ONNXBuilder(
         checkpoint_path=checkpoint_path,
+        model=model,
+        size=size,
+        size_divisible=size_divisible,
         score_thresh=score_thresh,
         nms_thresh=nms_thresh,
         version=version,
-        batch_size=batch_size,
         skip_preprocess=skip_preprocess,
         opset_version=opset_version,
     )
@@ -50,11 +60,14 @@ class ONNXBuilder:
 
     Args:
         checkpoint_path (string): Path of the custom trained YOLOv5 checkpoint.
+        model (nn.Module): The defined PyTorch module to be exported. Default: None
+        size: (Tuple[int, int]): the minimum and maximum size of the image to be rescaled.
+            Default: (640, 640)
+        size_divisible (int): stride of the models. Default: 32
         score_thresh (float): Score threshold used for postprocessing the detections.
             Default: 0.25
         nms_thresh (float): NMS threshold used for postprocessing the detections. Default: 0.45
         version (string): Upstream YOLOv5 version. Default: 'r6.0'
-        batch_size (int): Batch size for exporting YOLOv5.
         skip_preprocess (bool): Skip the preprocessing transformation when exporting the ONNX
             models. Default: False
         opset_version (int): Opset version for exporting ONNX models. Default: 11
@@ -62,51 +75,61 @@ class ONNXBuilder:
 
     def __init__(
         self,
-        checkpoint_path: str,
+        checkpoint_path: Optional[str] = None,
+        model: Optional[nn.Module] = None,
+        size: Tuple[int, int] = (640, 640),
+        size_divisible: int = 32,
         score_thresh: float = 0.25,
         nms_thresh: float = 0.45,
         version: str = "r6.0",
-        batch_size: int = 1,
         skip_preprocess: bool = False,
         opset_version: int = 11,
     ) -> None:
 
         super().__init__()
-        self.checkpoint_path = checkpoint_path
-        self.score_thresh = score_thresh
-        self.nms_thresh = nms_thresh
-        self.skip_preprocess = skip_preprocess
-        self.batch_size = batch_size
-        self.version = version
-        self.opset_version = opset_version
+        self._checkpoint_path = checkpoint_path
+        self._version = version
+        # For post-processing
+        self._score_thresh = score_thresh
+        self._nms_thresh = nms_thresh
+        self._skip_preprocess = skip_preprocess
+        # For pre-processing
+        self._size = size
+        self._size_divisible = size_divisible
+        # Define the module
+        if model is None:
+            model = self._build_model()
+        self.model = model
 
-        self.model = self.build_model()
+        self.opset_version = opset_version
         self.input_names = ["images"]
         self.output_names = ["scores", "labels", "boxes"]
-        self.input_sample = self.get_input_sample()
-        self.dynamic_axes = self.get_dynamic_axes()
+        self.input_sample = self._get_input_sample()
+        self.dynamic_axes = self._get_dynamic_axes()
 
-    def build_model(self):
-        if self.skip_preprocess:
+    def _build_model(self):
+        if self._skip_preprocess:
             model = YOLO.load_from_yolov5(
-                self.checkpoint_path,
-                score_thresh=self.score_thresh,
-                nms_thresh=self.nms_thresh,
-                version=self.version,
+                self._checkpoint_path,
+                score_thresh=self._score_thresh,
+                nms_thresh=self._nms_thresh,
+                version=self._version,
             )
         else:
             model = YOLOv5.load_from_yolov5(
-                self.checkpoint_path,
-                score_thresh=self.score_thresh,
-                nms_thresh=self.nms_thresh,
-                version=self.version,
+                self._checkpoint_path,
+                size=self._size,
+                size_divisible=self._size_divisible,
+                score_thresh=self._score_thresh,
+                nms_thresh=self._nms_thresh,
+                version=self._version,
             )
 
         model = model.eval()
         return model
 
-    def get_dynamic_axes(self):
-        if self.skip_preprocess:
+    def _get_dynamic_axes(self):
+        if self._skip_preprocess:
             return {
                 "images": {0: "batch", 2: "height", 3: "width"},
                 "boxes": {0: "batch", 1: "num_objects"},
@@ -121,22 +144,21 @@ class ONNXBuilder:
                 "scores": {0: "num_objects"},
             }
 
-    def get_input_sample(self):
-        if self.skip_preprocess:
-            return torch.rand(self.batch_size, 3, 640, 640)
+    def _get_input_sample(self):
+        if self._skip_preprocess:
+            return torch.rand(1, 3, 640, 640)
         else:
-            return [torch.rand(3, 640, 640)] * self.batch_size
+            return [torch.rand(3, 640, 640)]
 
     @torch.no_grad()
-    def to_onnx(self, onnx_path: Optional[str], **kwargs):
+    def to_onnx(self, onnx_path: str, **kwargs):
         """
         Saves the model in ONNX format.
 
         Args:
-            onnx_path (string, optional): The path to the ONNX graph to load. Default: None
+            onnx_path (string): The path to the ONNX graph to be exported.
             **kwargs: Will be passed to torch.onnx.export function.
         """
-        onnx_path = onnx_path or Path(self.checkpoint_path).with_suffix(".onnx")
 
         torch.onnx.export(
             self.model,
