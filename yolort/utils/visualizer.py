f35d002632f1ca746001f13efa10b7d8d022f49f
Zhiqiang Wang <zhiqwang@foxmail.com>
2022-03-01 17:18:00 +0800

    Use OpenCV to draw bounding boxes

--- yolort/utils/visualizer.py
+++ yolort/utils/visualizer.py
@@ -1,11 +1,15 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# Copyright (c) 2022, yolort team. All rights reserved.
+
 from enum import Enum, unique
 from typing import List, Optional, Tuple, Union
 
+import cv2
 import matplotlib.figure as mplfigure
 import numpy as np
 import torch
 from matplotlib.backends.backend_agg import FigureCanvasAgg
-from PIL import Image, ImageColor, ImageDraw, ImageFont
+from torch import Tensor
 
 
 @unique
@@ -32,7 +36,7 @@ class ColorMode(Enum):
 
 
 class VisImage:
-    def __init__(self, img, scale=1.0):
+    def __init__(self, img: np.ndarray, scale: float = 1.0):
         """
         Args:
             img (ndarray): an RGB image of shape (H, W, 3) in range [0, 255].
@@ -105,11 +109,6 @@ class VisImage:
         return rgb.astype("uint8")
 
 
-def _generate_color_palette(num_objects: int):
-    palette = torch.tensor([2 ** 25 - 1, 2 ** 15 - 1, 2 ** 21 - 1])
-    return [tuple((i * palette) % 255) for i in range(num_objects)]
-
-
 class Visualizer:
     """
     Visualizer that draws data about detection/segmentation on images.
@@ -134,46 +133,57 @@ class Visualizer:
     designed to be used for real-time applications.
 
     Args:
-        img_rgb: a numpy array of shape (H, W, C), where H and W correspond to
-            the height and width of the image respectively. C is the number of
-            color channels. The image is required to be in RGB format since that
-            is a requirement of the Matplotlib library. The image is also expected
-            to be in the range [0, 255].
-        metadata (Metadata): dataset metadata (e.g. class names and colors)
+        image (Union[torch.Tensor, numpy.ndarray]): Tensor of shape (C x H x W) or ndarray of
+            shape (H x W x C) with dtype uint8.
         instance_mode (ColorMode): defines one of the pre-defined style for drawing
             instances on an image.
     """
 
-    def __init__(self, img_rgb, metadata=None, scale=1.0, instance_mode=ColorMode.IMAGE):
+    def __init__(
+        self,
+        image: Union[Tensor, np.ndarray],
+        scale: float = 1.0,
+        line_width: Optional[int] = None,
+    ):
 
-        self.img = np.asarray(img_rgb).clip(0, 255).astype(np.uint8)
+        if isinstance(image, torch.Tensor):
+            if image.dtype != torch.uint8:
+                raise ValueError(f"Tensor uint8 expected, got {image.dtype}")
+            if image.dim() != 3:
+                raise ValueError("Pass individual images, not batches")
+            if image.size(0) not in {1, 3}:
+                raise ValueError("Only grayscale and RGB images are supported")
+            # Handle Grayscale images
+            if image.size(0) == 1:
+                image = torch.tile(image, (3, 1, 1))
+            self.img = image.permute(1, 2, 0).cpu().numpy()
+        elif isinstance(image, np.ndarray):
+            if image.dtype != np.uint8:
+                raise ValueError(f"Numpy uint8 expected, got {image.dtype}")
+            if image.ndim != 3:
+                raise ValueError("Currently only RGB images are supported")
+            self.img = image
+        else:
+            raise TypeError(f"Tensor or numpy.ndarray expected, got {type(image)}")
+
         self.output = VisImage(self.img, scale=scale)
         self.cpu_device = torch.device("cpu")
-
-        # too small texts are useless, therefore clamp to 9
-        self._default_font_size = max(np.sqrt(self.output.height * self.output.width) // 90, 10 // scale)
-        self._instance_mode = instance_mode
+        self.line_width = line_width or max(round(sum(self.img.shape) / 2 * 0.003), 2)
 
     @torch.no_grad()
     def draw_bounding_boxes(
-        image: torch.Tensor,
+        self,
         boxes: torch.Tensor,
         labels: Optional[List[str]] = None,
         colors: Optional[Union[List[Union[str, Tuple[int, int, int]]], str, Tuple[int, int, int]]] = None,
-        fill: Optional[bool] = False,
-        width: int = 1,
-        font: Optional[str] = None,
-        font_size: int = 10,
+        txt_colors: Tuple[int, int, int] = (255, 255, 255),
     ) -> torch.Tensor:
         """
         Draws bounding boxes on given image.
         The values of the input image should be uint8 between 0 and 255.
         If fill is True, Resulting Tensor should be saved as PNG image.
 
-        Adapted from https://github.com/pytorch/vision/blob/1fc53b2/torchvision/utils.py#L159-L195
-
         Args:
-            image (Tensor): Tensor of shape (C x H x W) and dtype uint8.
             boxes (Tensor): Tensor of size (N, 4) containing bounding boxes in (xmin, ymin, xmax, ymax)
                 format. Note that the boxes are absolute coordinates with respect to the image. In other
                 words: `0 <= xmin < xmax < W` and `0 <= ymin < ymax < H`.
@@ -193,64 +203,22 @@ class Visualizer:
             img (Tensor[C, H, W]): Image Tensor of dtype uint8 with bounding boxes plotted.
         """
 
-        if not isinstance(image, torch.Tensor):
-            raise TypeError(f"Tensor expected, got {type(image)}")
-        elif image.dtype != torch.uint8:
-            raise ValueError(f"Tensor uint8 expected, got {image.dtype}")
-        elif image.dim() != 3:
-            raise ValueError("Pass individual images, not batches")
-        elif image.size(0) not in {1, 3}:
-            raise ValueError("Only grayscale and RGB images are supported")
-
-        num_boxes = boxes.shape[0]
-
-        if labels is None:
-            labels: Union[List[str], List[None]] = [None] * num_boxes  # type: ignore[no-redef]
-        if len(labels) != num_boxes:
-            raise ValueError(
-                f"Number of boxes ({num_boxes}) and labels ({len(labels)}) mismatch. "
-                "Please specify labels for each box."
+        p1, p2 = (int(boxes[0]), int(boxes[1])), (int(boxes[2]), int(boxes[3]))
+        cv2.rectangle(self.img, p1, p2, colors, thickness=self.line_width, lineType=cv2.LINE_AA)
+        if labels:
+            tf = max(self.line_width - 1, 1)  # font thickness
+            w, h = cv2.getTextSize(labels, 0, fontScale=self.line_width / 3, thickness=tf)[0]
+            outside = p1[1] - h - 3 >= 0  # labels fits outside box
+            p2 = p1[0] + w, p1[1] - h - 3 if outside else p1[1] + h + 3
+            cv2.rectangle(self.img, p1, p2, colors, -1, cv2.LINE_AA)  # filled
+            cv2.putText(
+                self.img,
+                labels,
+                (p1[0], p1[1] - 2 if outside else p1[1] + h + 2),
+                0,
+                self.line_width / 3,
+                txt_colors,
+                thickness=tf,
+                lineType=cv2.LINE_AA,
             )
-
-        if colors is None:
-            colors = _generate_color_palette(num_boxes)
-        elif isinstance(colors, list):
-            if len(colors) < num_boxes:
-                raise ValueError(
-                    f"Number of colors ({len(colors)}) is less than number of boxes ({num_boxes}).",
-                )
-        else:  # colors specifies a single color for all boxes
-            colors = [colors] * num_boxes
-
-        colors = [(ImageColor.getrgb(color) if isinstance(color, str) else color) for color in colors]
-
-        # Handle Grayscale images
-        if image.size(0) == 1:
-            image = torch.tile(image, (3, 1, 1))
-
-        ndarr = image.permute(1, 2, 0).cpu().numpy()
-        img_to_draw = Image.fromarray(ndarr)
-        img_boxes = boxes.to(torch.int64).tolist()
-
-        if fill:
-            draw = ImageDraw.Draw(img_to_draw, "RGBA")
-        else:
-            draw = ImageDraw.Draw(img_to_draw)
-
-        try:
-            txt_font = ImageFont.truetype(font=font, size=font_size)
-        except IOError:
-            txt_font = ImageFont.load_default()
-
-        for bbox, color, label in zip(img_boxes, colors, labels):  # type: ignore[arg-type]
-            if fill:
-                fill_color = color + (100,)
-                draw.rectangle(bbox, width=width, outline=color, fill=fill_color)
-            else:
-                draw.rectangle(bbox, width=width, outline=color)
-
-            if label is not None:
-                margin = width + 1
-                draw.text((bbox[0] + margin, bbox[1] + margin), label, fill=color, font=txt_font)
-
-        return torch.from_numpy(np.array(img_to_draw)).permute(2, 0, 1).to(dtype=torch.uint8)
+        return self.img
