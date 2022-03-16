42600726908a4b688a162d1a36178050fd31ad41
Zhiqiang Wang <zhiqwang@foxmail.com>
2022-03-16 17:19:58 +0800

    Fix ImportError of loading load_from_ultralytics

--- yolort/relay/trt_inference.py
+++ yolort/relay/trt_inference.py
@@ -6,9 +6,9 @@ from typing import Optional, Tuple, Union
 import torch
 from torch import nn, Tensor
 from yolort.models import YOLO
+from yolort.models._utils import load_from_ultralytics
 from yolort.models.anchor_utils import AnchorGenerator
 from yolort.models.backbone_utils import darknet_pan_backbone
-from yolort.utils import load_from_ultralytics
 
 from .logits_decoder import LogitsDecoder
 
