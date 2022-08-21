7cdef76166b2497abf626d12ca74f2c4401f80e2
Zhiqiang Wang <zhiqwang@foxmail.com>
2022-08-22 02:35:47 +0800

    Add fully convolutional `Focus2` (#457)
    * Init Focus2 from rknn
    
    * Set filters as register_buffer

--- yolort/v5/models/common.py
+++ yolort/v5/models/common.py
@@ -16,6 +16,7 @@ import torch
 from PIL import Image
 from torch import nn, Tensor
 from torch.cuda import amp
+from torch.nn import functional as F
 from yolort.v5.utils.general import (
     colorstr,
     increment_path,
@@ -246,6 +247,39 @@ def space_to_depth(x: Tensor) -> Tensor:
     x = x.permute(0, 5, 3, 1, 2, 4)
     y = x.reshape(N, C * 4, H // 2, W // 2)
     return y
+
+
+class Focus2(nn.Module):
+    """
+    Is the Focus layer equivalent to a simple Conv layer?
+    https://github.com/ultralytics/yolov5/issues/4825#issue-998038464
+
+    Args:
+        c1 (int): ch_in
+        c2 (int): ch_out
+        k (int): kernel
+        s (int): stride
+        p (Optional[int]): padding
+        g (int): groups
+        act (bool or nn.Module): determine the activation function
+        version (str): Module version released by ultralytics. Possible values
+            are ["r3.1", "r4.0"]. Default: "r4.0".
+    """
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True, version="r4.0"):
+        super().__init__()
+        self.register_buffer("filter1", torch.tensor([[1, 0], [0, 0]]).float().expand(3, 1, 2, 2))
+        self.register_buffer("filter2", torch.tensor([[0, 0], [1, 0]]).float().expand(3, 1, 2, 2))
+        self.register_buffer("filter3", torch.tensor([[0, 1], [0, 0]]).float().expand(3, 1, 2, 2))
+        self.register_buffer("filter4", torch.tensor([[0, 0], [0, 1]]).float().expand(3, 1, 2, 2))
+        self.conv = Conv(c1 * 4, c2, k, s, p, g, act, version=version)
+
+    def forward(self, x: Tensor) -> Tensor:
+        conv1 = F.conv2d(x, self.filter1, stride=2, groups=3)
+        conv2 = F.conv2d(x, self.filter2, stride=2, groups=3)
+        conv3 = F.conv2d(x, self.filter3, stride=2, groups=3)
+        conv4 = F.conv2d(x, self.filter4, stride=2, groups=3)
+        return self.conv(torch.cat([conv1, conv2, conv3, conv4], dim=1))
 
 
 class Concat(nn.Module):
