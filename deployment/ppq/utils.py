9197d179c16537b78cab5c970c6afd039e52a660
Zhiqiang Wang <zhiqwang@foxmail.com>
2023-03-10 09:20:59 +0000

    refactoring

--- deployment/ppq/utils.py
+++ deployment/ppq/utils.py
@@ -1,25 +1,14 @@
-import argparse
-import datetime
 import os
-import time
 
-import onnx
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
 import torchvision
 import tqdm
 from PIL import Image
-from scipy import stats
-from torch.autograd import Function, Variable
-from torch.nn import Module, Parameter
-from torch.utils.data import DataLoader, Dataset
-from torchvision import datasets, transforms
 
-from yolort.models._checkpoint import load_from_ultralytics
-from yolort.models.backbone_utils import darknet_pan_backbone
-from yolort.models.box_head import YOLOHead
+from torch import nn, optim
+
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
 
 
 class Uniform_dataset(Dataset):
@@ -64,7 +53,7 @@ def get_distill_data(path, teacher_model, size, batch_size, start_idx, iteration
 
     eps = 1e-6
     # init hooks and single precision model
-    hooks, hook_handles, bn_stats, refined_gaussian = [], [], [], []
+    hooks, hook_handles, bn_stats = [], [], []
     teacher_model = teacher_model.cuda()
     teacher_model = teacher_model.eval()
 
@@ -94,7 +83,7 @@ def get_distill_data(path, teacher_model, size, batch_size, start_idx, iteration
         # init criterion, optimizer, scheduler
         gaussian_data = gaussian_data.cuda()
         gaussian_data.requires_grad = True
-        crit = nn.CrossEntropyLoss().cuda()
+
         optimizer = optim.Adam([gaussian_data], lr=0.5)
         scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, min_lr=1e-4, verbose=False, patience=100)
         input_mean = torch.zeros(1, 3).cuda()
@@ -105,8 +94,8 @@ def get_distill_data(path, teacher_model, size, batch_size, start_idx, iteration
             optimizer.zero_grad()
             for hook in hooks:
                 hook.clear()
-            # print(f"gaussian_data.shape = {gaussian_data.shape}")
-            output = teacher_model(gaussian_data)
+
+            _ = teacher_model(gaussian_data)
             mean_loss = 0
             std_loss = 0
 
@@ -131,19 +120,11 @@ def get_distill_data(path, teacher_model, size, batch_size, start_idx, iteration
             total_loss.backward()
             optimizer.step()
             scheduler.step(total_loss.item())
-        # refined_gaussian.append(gaussian_data.detach().clone())
         gaussian_data = gaussian_data.clone().detach().to(torch.device("cpu"))
-        # print(gaussian_data)
-        # print(i)
         torchvision.utils.save_image(gaussian_data, os.path.join(path, f"{start_idx + i}.jpg"))
-
-        # print(f"gaussian_data.shape = {gaussian_data.shape}")
-        # print(f"len(refined_gaussian) = {len(refined_gaussian)}")
 
     for handle in hook_handles:
         handle.remove()
-
-    # return refined_gaussian
 
 
 class Quant_datasets(Dataset):
@@ -158,36 +139,24 @@ class Quant_datasets(Dataset):
     def __getitem__(self, index):
         image_path = self.images[index]
         image = Image.open(image_path)
-        # _, image_name = os.path.split(image_path)
         if self.transform:
             image = self.transform(image)
         return image
 
 
 def prepare_data_loaders(data_path, shape):
-    # normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-    #                                  std=[0.229, 0.224, 0.225])
-    # dataset = torchvision.datasets.ImageNet(
-    #     data_path, split="train", transform=transforms.Compose([
-    #         transforms.RandomResizedCrop(224),
-    #         transforms.RandomHorizontalFlip(),
-    #         transforms.ToTensor(),
-    #         normalize,
-    #     ]))
+
     dataset_test = Quant_datasets(
         data_path,
         transform=transforms.Compose(
             [
                 transforms.Resize(shape[1]),
-                # transforms.CenterCrop(224),
                 transforms.ToTensor(),
             ]
         ),
     )
-    # normalize,]))
 
     test_sampler = torch.utils.data.SequentialSampler(dataset_test)
-
     data_loader_test = torch.utils.data.DataLoader(dataset_test, batch_size=1, sampler=test_sampler)
 
     return data_loader_test
@@ -196,138 +165,8 @@ def prepare_data_loaders(data_path, shape):
 def calibrate(model, data_loader):
     model.eval()
     with torch.no_grad():
-        # for image, _ in data_loader:
         for image in data_loader:
             model(image)
-
-
-def get_parser():
-    parser = argparse.ArgumentParser("ptq tool.", add_help=True)
-
-    parser.add_argument(
-        "--checkpoint_path", type=str, default="./model/yolov5s.pt", help="The path of checkpoint weights"
-    )
-    parser.add_argument("--version", type=str, default="r6.0", help="opset version")
-    parser.add_argument("--threshold", type=float, default=0.25, help="threshold")
-    parser.add_argument("--distill_data", type=int, default=1, help="if you want to distill data")
-    parser.add_argument(
-        "--regenerate_data",
-        type=int,
-        default=1,
-        help="if you wangt to generate new data in place of old data",
-    )
-    parser.add_argument(
-        "--distilled_data_path", type=str, default="./distilled_data/", help="The path of distilled data"
-    )
-    parser.add_argument(
-        "--calibration_data_path",
-        type=str,
-        default="./distilled_data/",
-        help="The path of calibration data, if zeroq is not used, you should set it",
-    )
-    parser.add_argument("--export2onnx", type=int, default=1, help="if export .pth to .onnx")
-    parser.add_argument("--ptq", type=int, default=1, help="flag to ptq the model")
-    parser.add_argument("--show_error_cal", type=int, default=1, help="flag to show error cal")
-    parser.add_argument("--batch_size", type=int, default=1, help="batch size")
-    parser.add_argument("--num_of_batches", type=int, default=10, help="num of batches")
-    parser.add_argument("--distill_iterations", type=int, default=50, help="distill iterations")
-    parser.add_argument("--input_size", default=[3, 640, 640], type=int, help="input size")
-    parser.add_argument("--onnx_input_name", type=str, default="dummy_input", help="onnx input name")
-    parser.add_argument("--onnx_output_name", type=str, default="dummy_output", help="onnx output name")
-    parser.add_argument(
-        "--onnx_output_path", type=str, default="./model/float_yolov5.onnx", help="onnx output name"
-    )
-    parser.add_argument(
-        "--sim_onnx_output_path",
-        type=str,
-        default="./model/sim_float_yolov5.onnx",
-        help="simed onnx output name",
-    )
-    parser.add_argument(
-        "--quantized_onnx_output_path",
-        type=str,
-        default="./model/quantized_yolov5.onnx",
-        help="quantized onnx output name",
-    )
-    parser.add_argument(
-        "--quantized_onnx_json_path",
-        type=str,
-        default="./model/quantized_yolov5.json",
-        help="quantized onnx output name",
-    )
-    parser.add_argument("--opset_version", type=int, default=11, help="opset version")
-    parser.add_argument("--device", type=str, default="cuda", help="opset version")
-    parser.add_argument("--calib_steps", type=int, default=64, help="opset version")
-
-    return parser
-
-
-class YOLO(nn.Module):
-    def __init__(self, backbone: nn.Module, strides, num_anchors, num_classes: int):
-        super().__init__()
-        self.backbone = backbone
-        self.head = YOLOHead(backbone.out_channels, num_anchors, strides, num_classes)
-
-    def forward(self, samples):
-
-        # get the features from the backbone
-        features = self.backbone(samples)
-
-        # compute the yolo heads outputs using the features
-        head_outputs = self.head(features)
-        return head_outputs
-
-
-class Model_wrapper(torch.nn.Module):
-    """
-    Wrapper class for model with dict/list rvalues.
-    """
-
-    def __init__(self, model: torch.nn.Module) -> None:
-        """
-        Init call.
-        """
-        super().__init__()
-        self.model = model
-
-    def forward(self, input_x):
-        """
-        Wrap forward call.
-        """
-        data = self.model(input_x)
-
-        if isinstance(data, dict):
-            data_named_tuple = namedtuple("ModelEndpoints", sorted(data.keys()))  # type: ignore
-            data = data_named_tuple(**data)  # type: ignore
-
-        elif isinstance(data, list):
-            data = tuple(data)
-
-        return data
-
-
-def make_model(checkpoint_path, version):
-
-    model_info = load_from_ultralytics(checkpoint_path, version=version)
-
-    backbone_name = f"darknet_{model_info['size']}_{version.replace('.', '_')}"
-    depth_multiple = model_info["depth_multiple"]
-    width_multiple = model_info["width_multiple"]
-    use_p6 = model_info["use_p6"]
-    backbone = darknet_pan_backbone(
-        backbone_name, depth_multiple, width_multiple, version=version, use_p6=use_p6
-    )
-    strides = model_info["strides"]
-    num_anchors = len(model_info["anchor_grids"][0]) // 2
-    num_classes = model_info["num_classes"]
-    model = YOLO(backbone, strides, num_anchors, num_classes)
-
-    model.load_state_dict(model_info["state_dict"])
-    model = Model_wrapper(model)
-
-    model = model.eval()
-
-    return model
 
 
 def collate_fn(batch):
